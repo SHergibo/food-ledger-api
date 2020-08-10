425e4c6dfd21f4dfe8a45851d6bfb174becc947c
SHergibo <sachau888@hotmail.com>
2020-08-10 13:28:28 +0200

    Ajout helmet dependencies

--- config/app.config.js
+++ config/app.config.js
@@ -1,27 +1,30 @@
 const Express = require('express'),
-      Morgan = require('morgan'),
-      Cors = require('cors'),
-      Compression = require('compression'),
-      BodyParser = require('body-parser'),
-      Router = require('./../api/routes/v1'),
-      Passport = require('passport'),
-      Strategies = require('./passport.config'),
-      ServiceErrorHandler = require('../api/services/error-handler.service');
+    Morgan = require('morgan'),
+    Cors = require('cors'),
+    Helmet = require('helmet');
+    Compression = require('compression'),
+    BodyParser = require('body-parser'),
+    Router = require('./../api/routes/v1'),
+    Passport = require('passport'),
+    Strategies = require('./passport.config'),
+    ServiceErrorHandler = require('../api/services/error-handler.service');
 
 const { HTTPLogs, api, env, environments } = require('./environment.config');
 
 const app = Express();
 
-app.use( Compression() );
+app.use(Helmet());
 
-app.use( Express.static('public'));
+app.use(Compression());
 
-app.use( BodyParser.json() );
-app.use( BodyParser.urlencoded({extended:true}));
+app.use(Express.static('public'));
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
 
 app.use(Cors());
 
-app.use( Passport.initialize() );
+app.use(Passport.initialize());
 
 Passport.use('jwt', Strategies.jwt);
 
@@ -29,10 +32,10 @@ app.use(`/api/${api}`, Router);
 
 app.use(Morgan(HTTPLogs));
 
-if(env.toUpperCase() === environments.DEVELOPMENT) {
+if (env.toUpperCase() === environments.DEVELOPMENT) {
     app.use(ServiceErrorHandler.exit);
     app.use(ServiceErrorHandler.notFound);
-}else{
+} else {
     app.use(ServiceErrorHandler.log, ServiceErrorHandler.exit);
     app.use(ServiceErrorHandler.notFound);
 }
