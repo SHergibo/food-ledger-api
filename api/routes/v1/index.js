d12d796f67de679545cd45514ebb2081be2c9a84
SHergibo <sachau888@hotmail.com>
2020-11-11 16:23:27 +0100

    fix: Add ShoppingList routes un index.js.

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -8,6 +8,7 @@ const Express = require('express'),
       HistoricRoutes = require('./historic.route'),
       BrandRoutes = require('./brand.route'),
       ProductLogRoutes = require('./product-log.route'),
+      ShoppingListRoutes = require('./shopping-list.route'),
       AuthRoutes = require('./auth.route'),
       TokenAuthRoutes = require('./token-auth.route');
 
@@ -23,6 +24,7 @@ router.use('/products', ProductRoutes);
 router.use('/historics', HistoricRoutes);
 router.use('/brands', BrandRoutes);
 router.use('/product-logs', ProductLogRoutes);
+router.use('/shopping-lists', ShoppingListRoutes);
 router.use('/tokens', TokenAuthRoutes);
 
 module.exports = router;
