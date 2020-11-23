185b60280590fb8d8879b18944995a9747c2cc48
SHergibo <sachau888@hotmail.com>
2020-11-23 15:58:47 +0100

    feat: Add route and controler to send shopping list via nodeMailer.

--- api/controllers/shopping-list.controller.js
+++ api/controllers/shopping-list.controller.js
@@ -1,6 +1,7 @@
 const ShoppingList = require('./../models/shopping-list.model'),
       Household = require('./../models/household.model'),
       FindByQueryHelper = require('./../helpers/findByQueryParams.helper'),
+      NodeMailer = require('./../helpers/nodemailer.helper'),
       Boom = require('@hapi/boom');
 
 /**
@@ -36,6 +37,56 @@ exports.removeAll = async (req, res, next) => {
   try {
     const household = await Household.findOne({ householdCode: req.params.householdCode });
     await ShoppingList.deleteMany({householdId : household._id});
+    return res.status(200).send();
+  } catch (error) {
+    next(Boom.badImplementation(error.message));
+  }
+};
+
+/**
+* Send mail shopping list
+*/
+exports.sendMail = async (req, res, next) => {
+  try {
+    const household = await Household.findOne({ householdCode: req.params.householdCode });
+    let shoppingList = await ShoppingList.find({householdId : household._id})
+    .populate({
+      path: 'product',
+      populate : {
+        path: 'brand',
+        select: {brandName: 1}
+      },
+      select: {
+        name: 1,
+        weight: 1,
+      }
+    })
+    .populate({
+      path: 'historic',
+      populate : {
+        path: 'brand',
+        select: {brandName: 1}
+      },
+      select: {
+        name: 1,
+        weight: 1,
+      }
+    });
+
+    if(shoppingList.length >= 1){
+  
+      let list = shoppingList.map(shopping => {
+        return `<li>${shopping.product.name} - ${shopping.product.brand.brandName.label} - ${shopping.product.weight}gr - ${shopping.numberProduct}</li>`;
+      }).join('');
+  
+      let output = `<h2>Voici votre liste de course à faire pour votre stock</h2>
+        <ul>
+        ${list}
+        </ul>
+      `;
+  
+      NodeMailer.send(output, 'Votre liste de course pour votre stock !');
+    }
     return res.status(200).send();
   } catch (error) {
     next(Boom.badImplementation(error.message));
--- api/routes/v1/shopping-list.route.js
+++ api/routes/v1/shopping-list.route.js
@@ -9,14 +9,18 @@ const router = Express.Router();
 
 router
   .route('/pagination/:householdCode')
-      .get(authorize([ADMIN], LOGGED_USER), checkSameHousehold, ShoppingListController.findPaginate);
+      .get(authorize([ADMIN, LOGGED_USER]), checkSameHousehold, ShoppingListController.findPaginate);
 
 router
   .route('/delete-pagination/:shoppingId')
-      .delete(authorize([ADMIN], LOGGED_USER), checkSameHousehold, ShoppingListController.removePagination);
+      .delete(authorize([ADMIN, LOGGED_USER]), checkSameHousehold, ShoppingListController.removePagination);
   
 router
   .route('/:householdCode')
-      .delete(authorize([ADMIN], LOGGED_USER), checkSameHousehold, ShoppingListController.removeAll);
+      .delete(authorize([ADMIN, LOGGED_USER]), checkSameHousehold, ShoppingListController.removeAll);
+  
+router
+    .route('/send-mail/:householdCode')
+        .get(authorize([ADMIN, LOGGED_USER]), checkSameHousehold, ShoppingListController.sendMail);
 
 module.exports = router;
