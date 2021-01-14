8207c355dfeaa51b5f89b3b0780f641e658132bb
SHergibo <sachau888@hotmail.com>
2021-01-14 17:01:59 +0100

    feat: Created statistic model and save and update statistic in chartData statistic.controller.

--- api/controllers/statistic.controller.js
+++ api/controllers/statistic.controller.js
@@ -1,4 +1,5 @@
-const Product = require('./../models/product.model'),
+const Statistic = require('./../models/statistic.model'),
+      Product = require('./../models/product.model'),
       Household = require('./../models/household.model'),
       Moment = require('moment');
       Boom = require('@hapi/boom');
@@ -8,54 +9,96 @@ const Product = require('./../models/product.model'),
 */
 exports.chartData = async (req, res, next) => {
   try {
+    
     const household = await Household.findOne({ householdCode: req.params.householdCode });
-    const Products = await Product.find({ householdId: household._id });
+    let statistic = await Statistic.findOne({ householdId: household._id });
+    const products = await Product.find({ householdId: household._id });
 
-    let dataFinal = {};
-    let dataChartOne = {};
-    let productType = ['legume', 'viande', 'poisson', 'fruit', 'boisson', 'produit-sucre', 'produit-laitier', 'farineux', 'cereale', 'legumineuse'];
-    let dataChartTwo = [0,0,0,0,0,0,0,0,0,0];
-    let dataChartThree = [0,0,0,0,0,0,0,0,0,0];
-    let dataChartFour = {};
-    let totalProduct = 0;
-
-    Products.forEach(product => {
-      //data for chart one
-      product.expirationDate.forEach(date => {
-        totalProduct = totalProduct + date.productLinkedToExpDate;
-        if(dataChartOne[date.expDate.getFullYear()]){
-          dataChartOne[date.expDate.getFullYear()][date.expDate.getMonth()] = dataChartOne[date.expDate.getFullYear()][date.expDate.getMonth()] + date.productLinkedToExpDate;
+    if(!statistic || statistic.isOutdated === true){
+      let dataFinal = {};
+      let dataChartOne = {};
+      let productType = ['legume', 'viande', 'poisson', 'fruit', 'boisson', 'produit-sucre', 'produit-laitier', 'farineux', 'cereale', 'legumineuse'];
+      let dataChartTwo = [0,0,0,0,0,0,0,0,0,0];
+      let dataChartThree = [0,0,0,0,0,0,0,0,0,0];
+      let dataChartFour = {};
+      let totalProduct = 0;
+  
+      products.forEach(product => {
+        //data for chart one
+        product.expirationDate.forEach(date => {
+          totalProduct = totalProduct + date.productLinkedToExpDate;
+          if(dataChartOne[date.expDate.getFullYear()]){
+            dataChartOne[date.expDate.getFullYear()][date.expDate.getMonth()] = dataChartOne[date.expDate.getFullYear()][date.expDate.getMonth()] + date.productLinkedToExpDate;
+          }else{
+            dataChartOne[date.expDate.getFullYear()] = [0,0,0,0,0,0,0,0,0,0,0,0];
+            dataChartOne[date.expDate.getFullYear()][date.expDate.getMonth()] = date.productLinkedToExpDate;
+          }
+        });
+  
+        let indexProductType = productType.indexOf(product.type.value);
+        //data for chart two
+        dataChartTwo[indexProductType] = dataChartTwo[indexProductType] + product.number;
+  
+        //data for chart three
+        dataChartThree[indexProductType] = dataChartThree[indexProductType] + (product.number * product.kcal);
+  
+      });
+  
+      //data for chart Four
+      let getYear = new Date().getFullYear();
+      let currentYear;
+      if(!statistic){
+        currentYear = [...Array(Moment().isoWeeksInYear()).fill(0)];
+        currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] = totalProduct;
+        dataChartFour[getYear] = currentYear;
+      }else{
+        dataChartFour = statistic.statistics.chartFour;
+        if(dataChartFour[getYear]){
+          currentYear = dataChartFour[getYear]
         }else{
-          dataChartOne[date.expDate.getFullYear()] = [0,0,0,0,0,0,0,0,0,0,0,0];
-          dataChartOne[date.expDate.getFullYear()][date.expDate.getMonth()] = date.productLinkedToExpDate;
+          currentYear = [...Array(Moment().isoWeeksInYear()).fill(0)];
         }
+        currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] = totalProduct;
+        dataChartFour[getYear] = currentYear;
+      }
+
+      dataFinal["chartOne"] = dataChartOne;
+      dataFinal["chartTwo"] = dataChartTwo;
+      dataFinal["chartThree"] = dataChartThree;
+      dataFinal["chartFour"] = dataChartFour;
+  
+      if(!statistic){
+        let stat = {
+          statistics : dataFinal,
+          householdId: household._id
+        }
+    
+        statistic = new Statistic(stat);
+        await statistic.save();
+      }
+      if(statistic.isOutdated === true){
+        statistic = await Statistic.findByIdAndUpdate(statistic._id, {statistics : dataFinal, isOutdated : false}, { override: true, upsert: true, new: true });
+      }
+    }
+
+    if(statistic && !statistic.isOutdated && !statistic.statistics.chartFour[new Date().getFullYear()]){
+      let totalProduct = 0;
+      products.forEach(product => {
+        product.expirationDate.forEach(date => {
+          totalProduct = totalProduct + date.productLinkedToExpDate;
+        });
       });
 
-      let indexProductType = productType.indexOf(product.type.value);
-      //data for chart two
-      dataChartTwo[indexProductType] = dataChartTwo[indexProductType] + product.number;
+      let dataChartFour = statistic.statistics.chartFour;
+      let currentYear = [...Array(Moment().isoWeeksInYear()).fill(0)];
+      currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] = totalProduct;
+      dataChartFour[new Date().getFullYear()] = currentYear;
 
-      //data for chart three
-      dataChartThree[indexProductType] = dataChartThree[indexProductType] + (product.number * product.kcal);
-
-    });
-
-    //!!!! Si on créer pour la première fois l'object ou qu'on est dans une nouvelle année !!!!
-    //data for chart Four
-    let getYear = new Date().getFullYear();
-    let newWeekInYearArray = [...Array(Moment().isoWeeksInYear()).fill(0)];
-    newWeekInYearArray[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] = totalProduct;
-    dataChartFour[getYear] = newYearArray;
-
-
-    dataFinal["chartOne"] = dataChartOne;
-    dataFinal["chartTwo"] = dataChartTwo;
-    dataFinal["chartThree"] = dataChartThree;
-    dataFinal["chartFour"] = dataChartFour;
-    console.log(dataFinal);
-    return res.json(dataFinal);
+      statistic = await Statistic.findByIdAndUpdate(statistic._id, {"statistics.chartFour" : dataChartFour}, { override: true, upsert: true, new: true });
+    }
+    
+    return res.json(statistic.transform());
   } catch (error) {
-    console.log(error);
     next(Boom.badImplementation(error.message));
   }
 };
---
+++ api/models/statistic.model.js
@@ -0,0 +1,49 @@
+const Mongoose = require('mongoose');
+
+let Schema = Mongoose.Schema;
+
+let statisticObject = new Schema({
+  chartOne: {
+    type: Object,
+  },
+  chartTwo: {
+    type: Array,
+  },
+  chartThree: {
+    type: Array,
+  },
+  chartFour: {
+    type: Object,
+  }
+}, { 
+  _id : false 
+});
+
+let schema = new Schema({
+  statistics: {
+    type : {statisticObject},
+    trim: true,
+    required: true
+  },
+  isOutdated: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  householdId: {
+    required: true,
+    type: Schema.Types.ObjectId,
+    ref: 'Household',
+  }
+});
+
+schema.methods.transform = function () {
+  const fields = ['statistics'];
+  const object = {};
+  fields.forEach((field) => {
+    object[field] = this[field];
+  });
+  return object;
+};
+
+module.exports = Mongoose.model('Statistics', schema);
