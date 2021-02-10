ce13cfa11fa6eedf901a14a2f678e252f3ccd680
SHergibo <sachau888@hotmail.com>
2021-02-10 15:48:33 +0100

    feat: Added userId firstname and lastname for notificationSended in finAll notification controller.

--- api/controllers/notification.controller.js
+++ api/controllers/notification.controller.js
@@ -1,10 +1,13 @@
 const Notification = require('./../models/notification.model'),
-  Boom = require('@hapi/boom'),
-  socketIo = require('./../../config/socket-io.config'),
-  SocketIoModel = require('./../models/socketIo.model');
+  Boom = require('@hapi/boom');
 
-  const transformNotificationArray = (notificationArray) => {
-    const fields = ['_id', 'message', 'fullName', 'senderUserCode', 'type', 'urlRequest', 'expirationDate'];
+  const transformNotificationArray = (notificationArray, withUserId = false) => {
+    let fields = ['_id', 'message', 'fullName', 'senderUserCode', 'type', 'urlRequest', 'expirationDate'];
+
+    if(withUserId){
+      fields.push('userId')
+    }
+    
     let arrayNotificationsTransformed = [];
     notificationArray.forEach((item) => {
       const object = {};
@@ -22,14 +25,19 @@ const Notification = require('./../models/notification.model'),
 exports.findAll = async (req, res, next) => {
   try {
     const notificationsReceived = await Notification.find({ userId: req.params.userId });
-    const notificationsSended = await Notification.find({ senderUserId: req.params.userId });
+    const notificationsSended = await Notification.find({ senderUserId: req.params.userId })
+    .populate({
+      path: 'userId',
+      select: 'firstname lastname -_id'
+    })
     
     let objectNotification ={
       notificationsReceived : transformNotificationArray(notificationsReceived),
-      notificationsSended : transformNotificationArray(notificationsSended)
+      notificationsSended : transformNotificationArray(notificationsSended, true)
     }
     return res.json(objectNotification);
   } catch (error) {
+    console.log(error);
     next(Boom.badImplementation(error.message));
   }
 };
