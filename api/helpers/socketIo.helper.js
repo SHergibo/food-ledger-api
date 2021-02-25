e6c659f0703ad384bf16bdae9ab943c4519765b1
SHergibo <sachau888@hotmail.com>
2021-02-25 14:25:20 +0100

    fix: Added try catch for socketIoEmit in socketIo helper.

--- api/helpers/socketIo.helper.js
+++ api/helpers/socketIo.helper.js
@@ -2,24 +2,29 @@ const socketIo = require('./../../config/socket-io.config'),
       SocketIoModel = require('./../models/socketIo.model');
 
 exports.socketIoEmit = async (userId, arrayEmitData) => {
-  let socketIoDb = await SocketIoModel.findOne({ userId: userId });
-  if(socketIoDb){
-    const io = socketIo.getSocketIoInstance();
-    arrayEmitData.forEach(emitData => {
-      let data = emitData.data;
-
-      if(emitData.name === "notifSocketIo"){
-        const fields = ['_id', 'message', 'fullName', 'senderUserCode', 'type', 'urlRequest', 'expirationDate'];
-        const notificationTransform = {};
-        fields.forEach((field)=>{
-          notificationTransform[field] = emitData.data[field];
-        });
-        data = notificationTransform;
-      }
-
-      io.to(socketIoDb.socketId).emit(emitData.name, data);
-
-    });
+  try {
+    let socketIoDb = await SocketIoModel.findOne({ userId: userId });
+    if(socketIoDb){
+      const io = socketIo.getSocketIoInstance();
+      arrayEmitData.forEach(emitData => {
+        let data = emitData.data;
+  
+        if(emitData.name === "notifSocketIo"){
+          const fields = ['_id', 'message', 'fullName', 'senderUserCode', 'type', 'urlRequest', 'expirationDate'];
+          const notificationTransform = {};
+          fields.forEach((field)=>{
+            notificationTransform[field] = emitData.data[field];
+          });
+          data = notificationTransform;
+        }
+  
+        io.to(socketIoDb.socketId).emit(emitData.name, data);
+  
+      });
+    }
+    return;
+  } catch (error) {
+    return error;
   }
-  return;
+
 };
