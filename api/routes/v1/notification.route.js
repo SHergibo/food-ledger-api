d63ec7f7ef44664abedc3fffb85b3f7adece352b
SHergibo <sachau888@hotmail.com>
2021-03-29 17:15:44 +0200

    fix: Added authorizations in notification.route.

--- api/routes/v1/notification.route.js
+++ api/routes/v1/notification.route.js
@@ -1,17 +1,16 @@
 const Express = require('express'),
-      NotificationController = require(`${process.cwd()}/api/controllers/notification.controller`);
-
-
+      NotificationController = require(`${process.cwd()}/api/controllers/notification.controller`),
+      { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth.middleware');
 
 const router = Express.Router();
 
 router
     .route('/:userId')
-        .get( NotificationController.findAll);
+        .get(authorize([ADMIN, LOGGED_USER]), NotificationController.findAll);
 
 router
     .route('/:notificationId')
-        .delete( NotificationController.remove);
+        .delete(authorize([ADMIN, LOGGED_USER]), NotificationController.remove);
 
 
 module.exports = router;
