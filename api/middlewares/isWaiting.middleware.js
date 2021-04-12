122496a7ea4442463a09add0d337c907451ac9f9
SHergibo <sachau888@hotmail.com>
2021-04-12 17:14:13 +0200

    feat: Created isWaiting middleware.

---
+++ api/middlewares/isWaiting.middleware.js
@@ -0,0 +1,10 @@
+const Boom = require('@hapi/boom');
+
+exports.isWaiting = async (req, res, next) => {
+  try {
+    let household = req.res.locals.householdData;
+    return household.isWaiting ? next(Boom.forbidden("Tant que votre famille n'a pas d'administrateur.trice, vous n'avez pas l'accès à cette fonction!")) : next();
+  } catch (error) {
+    next(Boom.badImplementation(error.message));
+  }
+};
