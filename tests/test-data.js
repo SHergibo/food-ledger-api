80c7afb89df592f5ef8f63682dbdd2538abaa7e1
SHergibo <sachau888@hotmail.com>
2021-04-27 17:02:56 +0200

    feat: Created test-data.js file.

---
+++ tests/test-data.js
@@ -0,0 +1,42 @@
+module.exports.adminOneDataComplete = {
+  firstname: 'John',
+  lastname: 'Doe',
+  email: 'johndoe@test.com',
+  password: '123456789',
+  role : 'admin',
+  householdName: "Familly-Doe"
+};
+
+module.exports.adminTwoDataComplete = {
+  firstname: 'David',
+  lastname: 'Doe',
+  email: 'daviddoe@test.com',
+  password: '123456789',
+  role : 'admin',
+  householdName: "Familly-DavidDoe"
+};
+
+module.exports.userTwoDataComplete = {
+  firstname: 'Sabine',
+  lastname: 'Doe',
+  email: 'sabinedoe@test.com',
+  password: '123456789',
+  role : 'user',
+};
+
+module.exports.userThreeDataComplete = {
+  firstname: 'Jules',
+  lastname: 'Doe',
+  email: 'julesjoe@test.com',
+  password: '123456789',
+  role : 'user',
+  householdName: "Familly-JulesDoe"
+};
+
+module.exports.userDataMissing = {
+  firstname: 'John',
+  lastname: 'Doe',
+  email: 'johndoe@test.com',
+  password: '123456789',
+  role : 'admin'
+};
