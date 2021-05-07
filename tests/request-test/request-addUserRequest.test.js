c4f536a5a2de396a7265729b1c11ff6c06fb2a88
SHergibo <sachau888@hotmail.com>
2021-05-07 15:19:45 +0200

    feat: Created test 1 in request-addUserRequest.test.

--- tests/request-test/request-addUserRequest.test.js
+++ tests/request-test/request-addUserRequest.test.js
@@ -1,11 +1,18 @@
-const { createAddUserRequestTest } = require('./request-helper/addUserRequest.helper'),
+const { createErrorTest, createAddUserRequestTest } = require('./request-helper/addUserRequest.helper'),
       { adminOneDataComplete, adminTwoDataComplete } = require('../test-data');
 
 const { dbManagement } = require('../db-management-utils');
 
 dbManagement();
 
-describe("Test simple add user request", () => {
+describe("Test addUserRequest controller", () => {
+  it("Test1 ) send add user request with bad usercode", async () => {
+    const { statusCode, error } = await createErrorTest(adminOneDataComplete, adminTwoDataComplete, "badUserCode");
+
+    expect(statusCode).toBe(400);
+    expect(error.isBoom).toBe(true);
+    expect(error.output.payload.message).toMatch("Code utilisateur non valide!");
+  });
   it("Send addUser request from admin to user and test if notification is created", async () => {
     const { user, addUser, householdAdmin, notificationAddUser } = await createAddUserRequestTest(adminOneDataComplete, adminTwoDataComplete);
 
@@ -15,3 +22,21 @@ describe("Test simple add user request", () => {
     expect(notificationAddUser.type).toBe("invitation-household-to-user");
   });
 });
+
+// Test error
+// 1) OK => test code utilisateur
+// 2) test code famille
+// 3) test famille est en isWaiting true
+// 4) test anti spam requête d'invitation
+// 5) test si utilisateur est déjà dans la famille
+// 6) test blocage envoie d'invite pour une personne dans une famille sans admin
+
+// création d'invitation
+// 1) création d'invitation household to user
+  // => test notification type household-to-user/need-switch-admin (en fonction du role de l'utilisateur user/admin et du n° de membres dans la famille si admin)
+  // => test message de la notification
+  // => test userId
+// 2) création d'invitation user to household
+  // => test notification type user-to-household
+  // => test message de la notification
+  // => test senderUserId
