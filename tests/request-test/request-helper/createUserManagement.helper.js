6683cbcafb1cdc59549c2844dcc45a1ea4824246
SHergibo <sachau888@hotmail.com>
2021-05-10 14:51:18 +0200

    feat: Created createUserManagement.helper.

---
+++ tests/request-test/request-helper/createUserManagement.helper.js
@@ -0,0 +1,43 @@
+const Household = require('../../../api/models/household.model'),
+      User = require('../../../api/models/user.model'),
+      Option = require('../../../api/models/option.model'),
+      cryptoRandomString = require('crypto-random-string');
+
+module.exports.createUser = async (userData) => {
+  let createdUser = await new User({
+    firstname: userData.firstname,
+    lastname: userData.lastname,
+    email: userData.email,
+    password: userData.password,
+    role: userData.role,
+    usercode: cryptoRandomString({length: 10, type: 'url-safe'}),
+  });
+  await createdUser.save();
+
+  let option = new Option({userId : createdUser._id});
+  await option.save();
+
+  createdUser = await User.findByIdAndUpdate(createdUser._id, { optionId: option._id }, { override: true, upsert: true, new: true });
+  return createdUser;
+};
+
+module.exports.createHousehold = async (userId, householdName) => {
+  let newHousehold = new Household({
+    members: [
+      {userData : userId, isFlagged: false}
+    ],
+    householdName: householdName,
+    userId: userId,
+    householdCode: cryptoRandomString({length: 10, type: 'url-safe'}),
+    lastChance : new Date(),
+  });
+  return await newHousehold.save();
+};
+
+module.exports.updateUserHouseholdId = async (userId, householdId) => {
+  return await User.findByIdAndUpdate(userId, { householdId: householdId }, { override: true, upsert: true, new: true });
+};
+
+module.exports.updateHouseholdMembers = async (householdId, membersArray, userId) => {
+  return await Household.findByIdAndUpdate(householdId, { members: [...membersArray, {isFlagged : false, userData : userId}] }, { override: true, upsert: true, new: true });
+};
