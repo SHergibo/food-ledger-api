06229d10906a1b863b6ff3cb6ea45b8d22443a6e
SHergibo <sachau888@hotmail.com>
2021-05-10 14:54:55 +0200

    Feat: Created switchADminRights.helper and added createErrorTest and switchAdminRightsRequest function inside it.

---
+++ tests/request-test/request-helper/switchAdminRights.helper.js
@@ -0,0 +1,91 @@
+const request = require("supertest"),
+      app = require("../../../config/app.config"), 
+      { api } = require('../../../config/environment.config'),
+      { login } = require('../../login.helper'),
+      Notification = require('../../../api/models/notification.model'),
+      {adminOneDataComplete, userTwoDataComplete} = require('../../test-data'),
+      {createUser, createHousehold, updateUserHouseholdId, updateHouseholdMembers} = require('./createUserManagement.helper');
+
+const switchAdminRightsRequest = async (objectData, accessToken) => {
+  return await request(app)
+  .post(`/api/${api}/requests/switch-admin-rights`)
+  .send(objectData)
+  .set('Authorization', `Bearer ${accessToken}`);
+};
+
+const createUsers = async () => {
+  let adminOne = await createUser(adminOneDataComplete);
+  const householdOne = await createHousehold(adminOne._id, adminOneDataComplete.householdName);
+  adminOne = await updateUserHouseholdId(adminOne._id, householdOne._id);
+
+  let userTwo = await createUser(userTwoDataComplete);
+  userTwo = await updateUserHouseholdId(userTwo._id, householdOne._id);
+  householdTwo = await updateHouseholdMembers(householdOne._id, householdOne.members, userTwo._id);
+
+  return { adminOne, householdOne, userTwo };
+};
+
+const createNotif = async (objectNotifData) => {
+  let createRequestNotif = await new Notification(objectNotifData);
+  await createRequestNotif.save();
+};
+
+module.exports.createErrorTest = async (testName) => {
+  const { adminOne, householdOne, userTwo } = await createUsers();
+
+  const accessTokenAdminOne = await login(adminOneDataComplete.email, adminOneDataComplete.password);
+
+  let objectData = {
+    householdId : householdOne._id,
+    userId : userTwo._id
+  };
+
+  if(testName === "badHouseholdId") objectData.householdId = "606dad080ac1c22766b37a53";
+  if(testName === "badUserId") objectData.userId = "606dad080ac1c22766b37a53";
+
+  if(testName === "otherRequestAdminNotif") 
+    await createNotif({
+      message: "Vous avez été désigné.e comme nouvel.le administrateur.trice de cette famille par l'administrateur.trice actuel.le, acceptez-vous cette requête?",
+      householdId: householdOne._id,
+      userId: userTwo._id,
+      senderUserId: adminOne._id,
+      type: "request-admin",
+      urlRequest: "switch-admin-rights-respond",
+    });
+
+
+  if(testName === "otherNeedSwitchAdminNotif")
+    await createNotif({
+      message: `L'administrateur.trice de la famille vous invite à rejoindre sa famille. Acceptez-vous l'invitation? Si oui, il faudra déléguer vos droits d'administrations à un.e autre membre de votre famille avant de pouvoir changer de famille.`,
+      householdId: householdOne._id,
+      userId: adminOne._id,
+      type: "need-switch-admin",
+      urlRequest: "add-user-respond",
+    });
+
+  const response = await switchAdminRightsRequest(objectData, accessTokenAdminOne);
+
+  return {statusCode : response.statusCode, error : JSON.parse(response.error.text)};
+};
+
+module.exports.switchAdminRightsRequest = async () => {
+  const { adminOne, householdOne, userTwo } = await createUsers();
+
+  const accessTokenAdminOne = await login(adminOneDataComplete.email, adminOneDataComplete.password);
+
+  let objectData = {
+    householdId : householdOne._id,
+    userId : userTwo._id
+  };
+  const response = await switchAdminRightsRequest(objectData, accessTokenAdminOne);
+
+  const checkNotification = await Notification.findOne({
+    userId: userTwo._id,
+    householdId : householdOne._id,
+    senderUserId : adminOne._id,
+    type: "request-admin"
+  });
+
+  return {statusCode : response.statusCode, checkNotification, adminOne, householdOne, userTwo};
+};
+
