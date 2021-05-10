5ebf0b14e4a2b9ff8f48475f56c8380bb9b14a4b
SHergibo <sachau888@hotmail.com>
2021-05-10 16:54:30 +0200

    feat: Created test 1 in request-switchAdminRightsRespond.helper.

---
+++ tests/request-test/request-switchAdminRightsRespond.test.js
@@ -0,0 +1,43 @@
+const { createErrorTest } = require('./request-helper/createErrorTestRequest.helper'),
+      { adminOneDataComplete } = require('../test-data');
+
+const { dbManagement } = require('../db-management-utils');
+
+dbManagement();
+
+const URL_REQUEST = 'switch-admin-rights-respond';
+
+describe("Test switchAdminRightsRespond request controller", () => {
+  it("Test 1) send switchAdminRightsRespond request with a bad notification id", async () => {
+    const res = await createErrorTest(
+      adminOneDataComplete, 
+      URL_REQUEST,
+    );
+    
+    expect(res.statusCode).toBe(404);
+    expect(res.error.isBoom).toBe(true);
+    expect(res.error.output.payload.message).toMatch("Notification non trouvée!");
+  });
+});
+
+//Test error
+  //OK => Notification non trouvée
+  //=> Mauvaise notification
+  //=> besoin de la query param acceptedRequest
+  //=> besoin de yes ou no dans la query param
+
+//Test request accepted
+ // => test statusCode
+ // => ancien admin devient user
+ // => ancien user devient admin
+ // => test transform notification need-switch-admin en invite normal pour l'ancien admin
+ // => test transform notification invite normal en need-switch-admin pour le nouvel admin
+ // => test nouvel userId dans household
+ // => test nouvel admin en position une du tableau des membres de household
+ // => test request notification deleted
+
+//Test request rejected
+ // => test statusCode
+ // => test request notification deleted
+ // => test notification d'information du refus créer
+
