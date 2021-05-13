d8d8acb3750e66d0baa1e12c1c4b49518a51e39c
SHergibo <sachau888@hotmail.com>
2021-05-13 18:14:20 +0200

    feat: Added injectHouseholdName export function in transformNotification.helper.

--- api/helpers/transformNotification.helper.js
+++ api/helpers/transformNotification.helper.js
@@ -2,24 +2,29 @@ const Household = require('./../models/household.model'),
       Notification = require('./../models/notification.model'),
       { socketIoEmit } = require('./../helpers/socketIo.helper');
 
+const injectHouseholdNameNotification = (notification) => {
+  let message = notification.message;
+  message = message.replace('{householdName}', notification.householdId.householdName);
+  notification.message = message;
+  return notification;
+};
 
 const transformNotification = async (userId, type) => {
   let notifData = {};
   const searchedNotification = await Notification.find({userId : userId, type: type});
   if(searchedNotification.length >= 1){
     for (const notif of searchedNotification) {
-      const otherHousehold = await Household.findById(notif.householdId);
 
       notifData = {
         "invitation-household-to-user" :  {
-          message: `L'administrateur.trice de la famille ${otherHousehold.householdName} vous invite à rejoindre sa famille. Acceptez-vous l'invitation? Si oui, il faudra déléguer vos droits d'administrations à un.e autre membre de votre famille avant de pouvoir changer de famille.`,
+          message: `L'administrateur.trice de la famille {householdName} vous invite à rejoindre sa famille. Acceptez-vous l'invitation? Si oui, il faudra déléguer vos droits d'administrations à un.e autre membre de votre famille avant de pouvoir changer de famille.`,
           householdId: notif.householdId,
           userId: notif.userId,
           type: "need-switch-admin",
           urlRequest: "add-user-respond",
         },
         "need-switch-admin" : {
-          message: `L'administrateur.trice de la famille ${otherHousehold.householdName} vous invite à rejoindre sa famille. Acceptez-vous l'invitation?`,
+          message: `L'administrateur.trice de la famille {householdName} vous invite à rejoindre sa famille. Acceptez-vous l'invitation?`,
           householdId: notif.householdId,
           userId: notif.userId,
           type: "invitation-household-to-user",
@@ -30,12 +35,18 @@ const transformNotification = async (userId, type) => {
       let newTranformedNotif = await new Notification(notifData[type]);
       await newTranformedNotif.save(); 
 
+      newTranformedNotif = await Notification.findById(newTranformedNotif._id)
+      .populate({
+        path: 'householdId',
+        select: 'householdName -_id'
+      });
+
       await Notification.findByIdAndDelete(notif._id);
 
       socketIoEmit(notif.userId, 
         [
           {name : "deleteNotificationReceived", data: notif._id},
-          {name : "updateNotificationReceived", data: newTranformedNotif.transform()},
+          {name : "updateNotificationReceived", data: injectHouseholdNameNotification(newTranformedNotif.transform({withHouseholdId: true}))},
         ]
       );
 
@@ -45,10 +56,12 @@ const transformNotification = async (userId, type) => {
         select: 'firstname lastname -_id'
       });
 
+      const otherHousehold = await Household.findById(notif.householdId);
+      
       socketIoEmit(otherHousehold.userId, 
         [
           {name : "deleteNotificationSended", data: notif._id},
-          {name : "updateNotificationSended", data: notificationSended.transform(true)},
+          {name : "updateNotificationSended", data: notificationSended.transform({withUserId : true})},
         ]
       ); 
     }
@@ -61,4 +74,8 @@ module.exports.transformInviteToNeedSwitchAdminNotif = async (userId) => {
 
 module.exports.transformNeedSwitchAdminToInviteNotif = async (userId) => {
   await transformNotification(userId, "need-switch-admin");
+};
+
+module.exports.injectHouseholdName = (notification) => {
+  return injectHouseholdNameNotification(notification);
 };
