5c57e36635947665e201c88e4e58473888de91bc
SHergibo <sachau888@hotmail.com>
2021-05-14 16:39:09 +0200

    fix: Used socketIo room to send data instead of using databate to fetch socketIoDb in socketIo.helper.js

--- api/helpers/socketIo.helper.js
+++ api/helpers/socketIo.helper.js
@@ -1,23 +1,17 @@
-const socketIo = require('./../../config/socket-io.config'),
-      SocketIoModel = require('./../models/socketIo.model');
+const socketIo = require('./../../config/socket-io.config');
 
 exports.socketIoEmit = async (userId, arrayEmitData) => {
   try {
-    let socketIoDb = await SocketIoModel.findOne({ userId: userId });
-    if(socketIoDb){
-      const io = socketIo.getSocketIoInstance();
+    const io = socketIo.getSocketIoInstance();
+    let userConnected = io.sockets.adapter.rooms.get(userId.toString());
+    if(userConnected){
       arrayEmitData.forEach(emitData => {
         let data = emitData.data;
-  
-        socketIoDb.socketId.forEach(socketId => {
-          io.to(socketId).emit(emitData.name, data);
-        });
-  
+        io.to(userId.toString()).emit(emitData.name, data);
       });
     }
     return;
   } catch (error) {
     return error;
   }
-
 };
