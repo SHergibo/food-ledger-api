0d611d38dc1fc62753d3bc605ea05fec2748c567
SHergibo <sachau888@hotmail.com>
2021-05-15 16:05:25 +0200

    fix: Deleted useless scripts controller in script.controller.

--- api/controllers/script.controller.js
+++ api/controllers/script.controller.js
@@ -8,66 +8,16 @@ const Household = require('./../models/household.model'),
 * Trigger launch script
 */
 exports.launch = async (req, res, next) => {
-  try {
-    const users = await User.find({}).lean();
+  // try {
+  //   const users = await User.find({}).lean();
 
-    users.forEach(async (user) => {
-      let household = await Household.findOne({householdCode: user.householdCode});
-      await User.findByIdAndUpdate(user._id, { householdId: household._id, $unset: { householdCode: 1 } });
-    });
+  //   users.forEach(async (user) => {
+  //     let household = await Household.findOne({householdCode: user.householdCode});
+  //     await User.findByIdAndUpdate(user._id, { householdId: household._id, $unset: { householdCode: 1 } });
+  //   });
 
-    return res.status(204).send();
-  } catch (error) {
-    next(Boom.badImplementation(error.message));
-  }
-};
-
-/**
-* Trigger householdMember script
-*/
-exports.householdMember = async (req, res, next) => {
-  try {
-    const households = await Household.find({}).lean();
-
-    households.forEach(async (household) => {
-      const member = household.member;
-      let members = [];
-      member.forEach((member) => {
-        let objectMember = {
-          userData : member.userId,
-          isFlagged : member.isFlagged,
-        }
-        members = [...members, objectMember];
-      });
-      await Household.findByIdAndUpdate(household._id, { members: members, $unset: { member: 1 } });
-    });
-
-    return res.status(204).send();
-  } catch (error) {
-    next(Boom.badImplementation(error.message));
-  }
-};
-
-/**
-* Test socketIo notification
-*/
-exports.testNotifSocketIo = async (req, res, next) => {
-  try {
-    const users = await User.find({});
-
-    users.forEach(async (user) => {
-      let newNotification = await new Notification({
-        message: `Test notification socketIo`,
-        type: 'information',
-        householdId: user.householdId,
-        userId: user._id,
-      });
-      await newNotification.save();
-      socketIoEmit(user._id, [{name : "updateNotificationReceived", data: newNotification.transform()}]);
-    });
-
-    return res.status(204).send();
-  } catch (error) {
-    next(Boom.badImplementation(error.message));
-  }
+  //   return res.status(204).send();
+  // } catch (error) {
+  //   next(Boom.badImplementation(error.message));
+  // }
 };
