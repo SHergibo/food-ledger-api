ccd41578e0da361916f9db18d8cf3c641c0b6ef3
SHergibo <sachau888@hotmail.com>
2021-06-17 17:30:40 +0200

    feat: Created findOne controller, added socketIoTo to update and remove controller in find.controller.

--- api/controllers/brand.controller.js
+++ api/controllers/brand.controller.js
@@ -1,6 +1,21 @@
 const Brand = require('./../models/brand.model'),
       { transformArray } = require('./../helpers/transformJsonData.helper'),
+      { socketIoTo } = require('./../helpers/socketIo.helper'),
       Boom = require('@hapi/boom');
+
+/**
+* GET one brand
+*/
+exports.findOne = async (req, res, next) => {
+  try {
+    const brand = await Brand.findById(req.params.brandId)
+    .sort({brandName : 1});
+
+    return res.json(brand.transform());
+  } catch (error) {
+    next(Boom.badImplementation(error.message));
+  }
+};
 
 /**
 * GET all brands
@@ -23,6 +38,7 @@ exports.update = async (req, res, next) => {
   try {
     //TODO faire attention à l'objet brandName et label et value en slugify
     const brand = await Brand.findByIdAndUpdate(req.params.brandId, req.body);
+    socketIoTo(`${brand.householdId}-brand`, "addedBrand", brand.transform());
     return res.json(brand.transform());
   } catch (error) {
     next(Boom.badImplementation(error.message));
@@ -35,6 +51,7 @@ exports.update = async (req, res, next) => {
 exports.remove = async (req, res, next) => {
   try {
     const brand = await Brand.findByIdAndDelete(req.params.brandId);
+    socketIoTo(`${brand.householdId}-brand`, "deletedBrand", brand._id);
     return res.json(brand.transform());
   } catch (error) {
     next(Boom.badImplementation(error.message));
