4c7f38abc8929e6a7e8314557166eecda3254f4c
SHergibo <sachau888@hotmail.com>
2021-06-25 15:25:35 +0200

    feat: Added console log error when env is Development in error-handler.service.

--- api/services/error-handler.service.js
+++ api/services/error-handler.service.js
@@ -1,33 +1,37 @@
-const {loggerError} = require('../../config/logger.config'),
+const { loggerError } = require('../../config/logger.config'),
       Notifier = require('node-notifier'),
+      { env, environments } = require('./../../config/environment.config');
       Boom = require('@hapi/boom');
 
-exports.log = (err, req, res, next) =>{
-    let error = {"message" : `Error in ${req.method} ${req.url}`, "stack": err.stack};
-    loggerError.error(error);
-    next(err);
+exports.log = (err, req, res, next) => {
+  let error = { "message": `Error in ${req.method} ${req.url}`, "stack": err.stack };
+  loggerError.error(error);
+  next(err);
 };
 
 exports.notify = (err, str, req) => {
-    let title = `Error in ${req.method} ${req.url}`;
-    Notifier.notify({
-        title : title,
-        message : str
-    })
+  let title = `Error in ${req.method} ${req.url}`;
+  Notifier.notify({
+    title: title,
+    message: str
+  })
 };
 
-exports.exit = (err, req, res, next) =>{
-    let code = 500;
-    if(err.output){
-        code = err.output.statusCode;
-    }else if (err.httpsStatusCode){
-        code = err.httpsStatusCode;
-    }
-    res.status(code);
-    res.json(err);
+exports.exit = (err, req, res, next) => {
+  let code = 500;
+  let errorBoom = err;
+  if (err.boom) errorBoom = err.boom;
+  if (errorBoom.output) code = errorBoom.output.statusCode;
+  if (errorBoom.httpsStatusCode) code = errorBoom.httpsStatusCode;
+  if (env.toUpperCase() === environments.DEVELOPMENT) {
+    if (err.error) console.log(err.error);
+    console.log(errorBoom);
+  }
+  res.status(code);
+  res.json(errorBoom);
 };
 
 exports.notFound = (req, res, next) => {
-    res.status(404);
-    res.json(Boom.notFound('End point not found'));
+  res.status(404);
+  res.json(Boom.notFound('End point not found'));
 }
