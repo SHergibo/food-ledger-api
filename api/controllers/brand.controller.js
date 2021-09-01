da31b438523b64846d2c08ea96504064fc06c629
SHergibo <sachau888@hotmail.com>
2021-09-01 15:54:22 +0200

    feat: Added findPaginate and removePagination controller in brand controller.

--- api/controllers/brand.controller.js
+++ api/controllers/brand.controller.js
@@ -1,5 +1,6 @@
 const Brand = require('./../models/brand.model'),
       { transformArray } = require('./../helpers/transformJsonData.helper'),
+      FindByQueryHelper = require('./../helpers/findByQueryParams.helper'),
       { socketIoTo } = require('./../helpers/socketIo.helper'),
       Boom = require('@hapi/boom');
 
@@ -32,6 +33,18 @@ exports.findAll = async (req, res, next) => {
 };
 
 /**
+* GET brand List with pagination
+*/
+exports.findPaginate = async (req, res, next) => {
+  try {
+    const finalObject = await FindByQueryHelper.finalObjectBrandList(req, req.params.householdId, Brand);
+    return res.json(finalObject);
+  } catch (error) {
+    next({error: error, boom: Boom.badImplementation(error.message)});
+  }
+};
+
+/**
 * PATCH one brand
 */
 exports.update = async (req, res, next) => {
@@ -53,6 +66,19 @@ exports.remove = async (req, res, next) => {
     const brand = await Brand.findByIdAndDelete(req.params.brandId);
     socketIoTo(`${brand.householdId}-brand`, "deletedBrand", brand._id);
     return res.json(brand.transform());
+  } catch (error) {
+    next({error: error, boom: Boom.badImplementation(error.message)});
+  }
+};
+
+/**
+* DELETE one brand and send new brand list using front-end pagination data
+*/
+exports.removePagination = async (req, res, next) => {
+  try {
+    const brand = await Brand.findByIdAndRemove(req.params.brandId);
+    const finalObject = await FindByQueryHelper.finalObjectBrandList(req, brand.householdId, Brand);
+    return res.json(finalObject);
   } catch (error) {
     next({error: error, boom: Boom.badImplementation(error.message)});
   }
