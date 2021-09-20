9492742f29692b56926959b854cf8158b534a4fb
SHergibo <sachau888@hotmail.com>
2021-09-20 15:12:18 +0200

    feat: Created /received-notification and /sended-notification route in notification.route.

--- api/routes/v1/notification.route.js
+++ api/routes/v1/notification.route.js
@@ -3,16 +3,20 @@ const router = require('express').Router(),
       { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth.middleware');
 
 router
-  .route('/:userId')
-    .get(authorize([ADMIN, LOGGED_USER]), NotificationController.findAll);
+  .route('/received-notification/:userId')
+    .get(authorize([ADMIN, LOGGED_USER]), NotificationController.findAllReceivedNotif);
 
 router
-  .route('/pagination-notification-received/:userId')
-    .get(authorize([ADMIN, LOGGED_USER]), NotificationController.findPaginateNotifReceived);
+  .route('/sended-notification/:userId')
+    .get(authorize([ADMIN, LOGGED_USER]), NotificationController.findAllSendedNotif);
 
 router
-  .route('/pagination-notification-sended/:userId')
-    .get(authorize([ADMIN, LOGGED_USER]), NotificationController.findPaginateNotifSended);
+  .route('/pagination-received-notification/:userId')
+    .get(authorize([ADMIN, LOGGED_USER]), NotificationController.findPaginateReceivedNotif);
+
+router
+  .route('/pagination-sended-notification/:userId')
+    .get(authorize([ADMIN, LOGGED_USER]), NotificationController.findPaginateSendedNotif);
 
 router
   .route('/:notificationId')
