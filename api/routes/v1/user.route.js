2c4df9d9c502a09d32b61b39b17eceb7007effcb
SHergibo <sachau888@hotmail.com>
2021-10-12 15:06:20 +0200

    feat: Created pagination route in user.route.js.

--- api/routes/v1/user.route.js
+++ api/routes/v1/user.route.js
@@ -1,10 +1,15 @@
 const router = require('express').Router(),
       UserController = require(`${process.cwd()}/api/controllers/user.controller`),
-      { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth.middleware');
+      { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth.middleware'),
+      { checkSameHousehold } = require('../../middlewares/sameHousehold.middleware');
 
 router
   .route('/')
     .post(UserController.add);
+
+router
+  .route('/pagination/:householdId')
+    .get(authorize([ADMIN, LOGGED_USER]), checkSameHousehold, UserController.findPaginate);
 
 router
   .route('/:userId')
