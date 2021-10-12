3f0664a29b323d69cb76a16fb2920c3c83257c61
SHergibo <sachau888@hotmail.com>
2021-10-12 15:07:03 +0200

    feat: Added user into objectFields in transformArray function in tranformJsonData.helper.

--- api/helpers/transformJsonData.helper.js
+++ api/helpers/transformJsonData.helper.js
@@ -7,7 +7,8 @@ exports.transformArray = (dataArray, fieldName) => {
     product: ['_id', 'name', 'brand', 'type', 'weight', 'kcal', 'expirationDate', 'location', 'number', 'minimumInStock', 'isBeingEdited'],
     brand: ['_id', 'brandName', 'numberOfProduct', "numberOfHistoric", "isBeingEdited"],
     productLog: ['_id', 'productName', 'productBrand', 'productWeight', 'infoProduct', 'numberProduct', 'householdId', 'user', 'createdAt'],
-    shoppingList: ['_id', 'product', 'historic', 'numberProduct', 'createdAt']
+    shoppingList: ['_id', 'product', 'historic', 'numberProduct', 'createdAt'],
+    user: ['_id', 'usercode', 'firstname', 'lastname', 'role']
   }
   
   let arrayTransformed = [];
