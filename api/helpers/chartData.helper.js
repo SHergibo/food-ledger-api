3e5997a068868f811f02441cd4dcb9c62d826820
SHergibo <sachau888@hotmail.com>
2021-10-18 14:21:56 +0200

    feat: Created chartData.helper file in helpers folder.

---
+++ api/helpers/chartData.helper.js
@@ -0,0 +1,95 @@
+const Statistic = require('../models/statistic.model'),
+      Product = require('../models/product.model'),
+      Moment = require('moment');
+
+exports.getChartData = async ({ householdId }) => {
+  let statistic = await Statistic.findOne({ householdId });
+  const products = await Product.find({ householdId });
+
+  if(products.length >= 1){
+    if(!statistic || statistic.isOutdated === true || !statistic.chartOne){
+      let dataFinal = {};
+      let dataChartOne = {};
+      let productType = ['legume', 'viande', 'poisson', 'fruit', 'boisson', 'produit-sucre', 'produit-laitier', 'farineux', 'cereale', 'legumineuse'];
+      let dataChartTwo = [0,0,0,0,0,0,0,0,0,0];
+      let dataChartThree = [0,0,0,0,0,0,0,0,0,0];
+      let dataChartFour = {};
+      let totalProduct = 0;
+  
+      products.forEach(product => {
+        //data for chart one
+        totalProduct = totalProduct + product.number;
+        product.expirationDate.forEach(date => {
+          if(dataChartOne[date.expDate.getFullYear()]){
+            dataChartOne[date.expDate.getFullYear()][date.expDate.getMonth()] = dataChartOne[date.expDate.getFullYear()][date.expDate.getMonth()] + date.productLinkedToExpDate;
+          }else{
+            dataChartOne[date.expDate.getFullYear()] = [0,0,0,0,0,0,0,0,0,0,0,0];
+            dataChartOne[date.expDate.getFullYear()][date.expDate.getMonth()] = date.productLinkedToExpDate;
+          }
+        });
+  
+        let indexProductType = productType.indexOf(product.type.value);
+        //data for chart two
+        dataChartTwo[indexProductType] = dataChartTwo[indexProductType] + product.number;
+  
+        //data for chart three
+        dataChartThree[indexProductType] = dataChartThree[indexProductType] + (product.number * product.kcal);
+  
+      });
+  
+      //data for chart Four
+      let getYear = new Date().getFullYear();
+      let currentYear;
+      if(!statistic){
+        currentYear = [...Array(Moment().isoWeeksInYear()).fill(0)];
+        currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] = totalProduct;
+        dataChartFour[getYear] = currentYear;
+      }else{
+        dataChartFour = statistic.statistics.chartFour;
+        if(dataChartFour[getYear]){
+          currentYear = dataChartFour[getYear]
+        }else{
+          currentYear = [...Array(Moment().isoWeeksInYear()).fill(0)];
+        }
+        currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] = totalProduct;
+        dataChartFour[getYear] = currentYear;
+      }
+
+      dataFinal["chartOne"] = dataChartOne;
+      dataFinal["chartTwo"] = dataChartTwo;
+      dataFinal["chartThree"] = dataChartThree;
+      dataFinal["chartFour"] = dataChartFour;
+  
+      if(!statistic){
+        let stat = {
+          statistics : dataFinal,
+          householdId
+        }
+    
+        statistic = new Statistic(stat);
+        await statistic.save();
+      }
+      if(statistic.isOutdated === true || !statistic.chartOne){
+        statistic = await Statistic.findByIdAndUpdate(statistic._id, {statistics : dataFinal, isOutdated : false});
+      }
+    }
+
+    if(statistic && !statistic.isOutdated && !statistic.statistics.chartFour[new Date().getFullYear()]){
+      let totalProduct = 0;
+      products.forEach(product => {
+        totalProduct = totalProduct + product.number;
+      });
+
+      let dataChartFour = statistic.statistics.chartFour;
+      let currentYear = [...Array(Moment().isoWeeksInYear()).fill(0)];
+      currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] = totalProduct;
+      dataChartFour[new Date().getFullYear()] = currentYear;
+
+      statistic = await Statistic.findByIdAndUpdate(statistic._id, {"statistics.chartFour" : dataChartFour});
+    }
+
+    return statistic.transform();
+  }else{
+    return {statistics:{}};
+  }
+};
