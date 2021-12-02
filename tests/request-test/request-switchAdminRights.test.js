449125bb3c9d61ff08dbc08a9c04003ae1aff9fc
SHergibo <sachau888@hotmail.com>
2021-12-02 16:03:55 +0100

    feat: Added socketIo testing in test 5 in request-switchAdminRights.test.js.

--- tests/request-test/request-switchAdminRights.test.js
+++ tests/request-test/request-switchAdminRights.test.js
@@ -1,6 +1,7 @@
-const { createErrorTest, switchAdminRightsRequest } = require('./request-helper/switchAdminRights.helper');
+const { createUsersSwitchAdminRights, createErrorTest, switchAdminRightsRequest } = require('./request-helper/switchAdminRights.helper');
 
 const { dbManagement } = require('../db-management-utils');
+const { connectSocketClient, disconnectSocketClient } = require('../socket-io-management-utils');
 
 dbManagement();
 
@@ -34,12 +35,43 @@ describe("Test switchAdminRights request controller", () => {
     expect(error.output.payload.message).toMatch("Cet.te utilisateur.trice n'existe pas!");
   });
   it("Test 5) send switch admin rights request", async () => {
-    const { statusCode, checkNotification, adminOne, householdOne, userTwo } = await switchAdminRightsRequest();
+    const { adminOne, householdOne, userTwo, objectClientSocket } = await createUsersSwitchAdminRights({withSocket : true});
+    connectSocketClient(objectClientSocket);
+
+    let updateNotifAdminOne;
+    objectClientSocket.clientSocketAdminOne.on("updateNotifArray", (data) => {
+      updateNotifAdminOne = data;
+    });
+    
+    let updateNotifRecievedUserTwo;
+    objectClientSocket.clientSocketUserTwo.on("updateNotificationReceived", (data) => {
+      updateNotifRecievedUserTwo = data;
+    });
+
+    let updateNotifUserTwo;
+    objectClientSocket.clientSocketUserTwo.on("updateNotifArray", (data) => {
+      updateNotifUserTwo = data;
+    });
+
+    const { statusCode, checkNotification } = await switchAdminRightsRequest({adminOne, householdOne, userTwo});
+
+    expect(updateNotifRecievedUserTwo._id.toString()).toBe(checkNotification._id.toString());
+    expect(updateNotifRecievedUserTwo.type).toBe(checkNotification.type);
+
+    expect(updateNotifUserTwo.arrayData[0]._id.toString()).toBe(checkNotification._id.toString());
+    expect(updateNotifUserTwo.arrayData[0].type).toBe(checkNotification.type);
+    expect(updateNotifUserTwo.totalNotifReceived).toBe(1);
+
+    expect(updateNotifAdminOne.arrayData[0]._id.toString()).toBe(checkNotification._id.toString());
+    expect(updateNotifAdminOne.arrayData[0].type).toBe(checkNotification.type);
+    expect(updateNotifAdminOne.totalNotifSended).toBe(1);
 
     expect(statusCode).toBe(200);
     expect(checkNotification.userId.toString()).toBe(userTwo._id.toString());
     expect(checkNotification.senderUserId.toString()).toBe(adminOne._id.toString());
     expect(checkNotification.householdId.toString()).toBe(householdOne._id.toString());
     expect(checkNotification.type).toMatch("request-admin");
+    
+    disconnectSocketClient(objectClientSocket);
   });
 });
