9964f1cf77e1694e71bf0baa82d92b757425753b
SHergibo <sachau888@hotmail.com>
2021-12-03 14:57:21 +0100

    feat: Added socketIo testing in test 6 in request-switchAdminRightsRespond.test.js.

--- tests/request-test/request-switchAdminRightsRespond.test.js
+++ tests/request-test/request-switchAdminRightsRespond.test.js
@@ -1,9 +1,10 @@
 const { createErrorTest } = require('./request-helper/createErrorTestRequest.helper'),
       { adminOneDataComplete, notificationDelegateAdmin, notificationRequestAdmin } = require('../test-data'),
-      { switchAdminRightsRequest } = require('./request-helper/switchAdminRights.helper'),
+      { createUsersSwitchAdminRights, switchAdminRightsRequest } = require('./request-helper/switchAdminRights.helper'),
       { switchAdminRightsRespondRequest } = require('./request-helper/switchAdminRightsRespond.helper');
 
 const { dbManagement } = require('../db-management-utils');
+const { connectSocketClient, disconnectSocketClient } = require('../socket-io-management-utils');
 
 dbManagement();
 
@@ -67,7 +68,10 @@ describe("Test switchAdminRightsRespond request controller", () => {
     expect(res.error.output.payload.message).toMatch("Paramètre de requête invalide!");
   });
   it("Test 6) accept request admin notification", async () => {
-    const { checkNotification, adminOne, userTwo, householdOne } = await switchAdminRightsRequest();
+    const { adminOne, householdOne, userTwo, objectClientSocket } = await createUsersSwitchAdminRights({withSocket : true});
+    connectSocketClient(objectClientSocket);
+
+    const { checkNotification } = await switchAdminRightsRequest({adminOne, householdOne, userTwo});
     const { 
       statusCode, 
       deletedNotification, 
@@ -91,9 +95,36 @@ describe("Test switchAdminRightsRespond request controller", () => {
     expect(userTwoNotifAfterUpdate).toBeNull();
     expect(adminOneNotifTransformed.userId.toString()).toBe(adminOne._id.toString());
     expect(userTwoNotifTransformed.userId.toString()).toBe(userTwo._id.toString());
+
+    disconnectSocketClient(objectClientSocket);
   });
   it("Test 7) reject request admin notification", async () => {
-    const { checkNotification, adminOne, userTwo, householdOne } = await switchAdminRightsRequest();
+    const { adminOne, householdOne, userTwo, objectClientSocket } = await createUsersSwitchAdminRights({withSocket : true});
+    connectSocketClient(objectClientSocket);
+
+    const { checkNotification } = await switchAdminRightsRequest({adminOne, householdOne, userTwo});
+
+    let updateNotifReceivedAdminOne;
+    objectClientSocket.clientSocketAdminOne.on("updateNotificationReceived", (data) => {
+      updateNotifReceivedAdminOne = data;
+    });
+     
+    let updateNotifAdminOne = {};
+    objectClientSocket.clientSocketAdminOne.on("updateNotifArray", (data) => {
+      if(Object.keys(data).find(key => key === 'totalNotifSended')) updateNotifAdminOne['updateSendedNotif'] = data;
+      if(Object.keys(data).find(key => key === 'totalNotifReceived')) updateNotifAdminOne['updateReceivedNotif'] = data;
+    });
+
+    let deleteNotifReceivedUserTwo;
+    objectClientSocket.clientSocketUserTwo.on("deleteNotificationReceived", (data) => {
+      deleteNotifReceivedUserTwo = data;
+    });
+
+    let updateNotifUserTwo;
+    objectClientSocket.clientSocketUserTwo.on("updateNotifArray", (data) => {
+      updateNotifUserTwo = data;
+    });
+
     const { 
       statusCode, 
       deletedNotification, 
@@ -107,6 +138,21 @@ describe("Test switchAdminRightsRespond request controller", () => {
       userTwoNotifTransformed,
       informationNotif
     } = await switchAdminRightsRespondRequest(checkNotification, adminOne, userTwo, householdOne, 'no');
+
+    expect(updateNotifReceivedAdminOne._id.toString()).toBe(updateNotifAdminOne.updateReceivedNotif.arrayData[1]._id.toString());
+    expect(updateNotifReceivedAdminOne.type).toBe(updateNotifAdminOne.updateReceivedNotif.arrayData[1].type);
+
+    expect(updateNotifAdminOne.updateReceivedNotif.arrayData[1].type).toBe("information");
+    expect(updateNotifAdminOne.updateReceivedNotif.totalNotifReceived).toBe(2);
+
+    expect(updateNotifAdminOne.updateSendedNotif.arrayData).toEqual([]);
+    expect(updateNotifAdminOne.updateSendedNotif.totalNotifSended).toBe(0);
+
+    expect(deleteNotifReceivedUserTwo).toBe(checkNotification._id.toString());
+
+    expect(updateNotifUserTwo.arrayData[0]._id.toString()).toBe(userTwoNotifAfterUpdate._id.toString());
+    expect(updateNotifUserTwo.arrayData[0].type).toBe(userTwoNotifAfterUpdate.type);
+    expect(updateNotifUserTwo.totalNotifReceived).toBe(1);
     
     expect(statusCode).toBe(204);
     expect(deletedNotification).toBeNull();
@@ -119,5 +165,7 @@ describe("Test switchAdminRightsRespond request controller", () => {
     expect(adminOneNotifTransformed).toBeNull()
     expect(userTwoNotifTransformed).toBeNull();
     expect(informationNotif.type).toMatch("information");
+
+    disconnectSocketClient(objectClientSocket);
   });
 });
