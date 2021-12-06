264dd7645e43229d2ce3e38829344a45091d441c
SHergibo <sachau888@hotmail.com>
2021-12-06 15:47:48 +0100

    feat: Created create-user-auth.helper.js file.

---
+++ tests/auth-test/auth-helper/create-user-auth.helper.js
@@ -0,0 +1,17 @@
+const { createUser } = require('../../global-helper/createUserManagement.helper'),
+      { adminOneDataComplete } = require('../../test-data');
+
+module.exports.createUser = async (withSocket) => {
+  let clientSocketAdminOne;
+  if(withSocket) clientSocketAdminOne = Client(`http://localhost:8003`);
+
+  let adminOne = await createUser({userData : adminOneDataComplete, clientSocket: clientSocketAdminOne});
+  adminOne.clearPasswordForTesting = adminOneDataComplete.password;
+
+  const objectClientSocket = { clientSocketAdminOne };
+
+  let returnedObject = { adminOne };
+  if(objectClientSocket.clientSocketAdminOne) returnedObject = {...returnedObject, objectClientSocket};
+
+  return returnedObject;
+};
