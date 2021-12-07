049ea6c333629b675084c0ad4416ca754d3d8236
SHergibo <sachau888@hotmail.com>
2021-12-07 14:44:19 +0100

    feat: Created checkRefreshToken and refactored code in check-token-auth.helper.js.

--- tests/auth-test/auth-helper/check-token-auth.helper.js
+++ tests/auth-test/auth-helper/check-token-auth.helper.js
@@ -2,17 +2,28 @@ const Jwt = require('jwt-simple'),
       { jwtSecret } = require('../../../config/environment.config'),
       RefreshToken = require('../../../api/models/refresh-token.model');
 
-module.exports.checkTokenDataAuth = async ({tokenData, userId}) => {
+
+const checkAuthAndRefreshToken = async ({tokenData, userId}) => {
   let objectReturn = {};
-  let jwtDecoded = Jwt.decode(tokenData.token.accessToken, jwtSecret);
+  let jwtDecoded = Jwt.decode(tokenData.accessToken, jwtSecret);
   jwtDecoded.sub === userId.toString() ? objectReturn.accessToken = true : objectReturn.accessToken = false;
 
-  tokenData.token.tokenType === "Bearer" ? objectReturn.type = true : objectReturn.type = false;
+  tokenData.tokenType === "Bearer" ? objectReturn.type = true : objectReturn.type = false;
 
-  let refreshToken = await RefreshToken.findById(tokenData.token.refreshToken._id);
+  let refreshToken = await RefreshToken.findById(tokenData.refreshToken._id);
   refreshToken ? objectReturn.refreshToken = true : objectReturn.refreshToken = false;
+
+  return objectReturn;
+};
+
+module.exports.checkTokenDataAuth = async ({tokenData, userId}) => {
+  let objectReturn = await checkAuthAndRefreshToken({tokenData: tokenData.token, userId});
 
   tokenData.user._id.toString() === userId.toString() ? objectReturn.user = true : objectReturn.user = false;
 
   return objectReturn;
+};
+
+module.exports.checkRefreshToken = async ({tokenData, userId}) => {
+  return await checkAuthAndRefreshToken({tokenData: tokenData, userId});
 };
