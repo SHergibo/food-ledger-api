6878dc8a6dbaa2689c2379dd454d5e8189a1f987
SHergibo <sachau888@hotmail.com>
2021-12-13 15:44:09 +0100

    fix: Fixed route condition in all if statements in basicRouteAuth function in route-auth.helper.js.

--- tests/auth-test/auth-helper/route-auth.helper.js
+++ tests/auth-test/auth-helper/route-auth.helper.js
@@ -5,9 +5,9 @@ module.exports.basicRouteAuth = async ({userCredentials, route, accessToken}) =>
     email :userCredentials.email
   };
 
-  if(route === "login" || route === "check-credential") sendedObject["password"] = userCredentials.password;
-  if(route === "refresh-token") sendedObject["refreshToken"] = userCredentials.refreshToken;
-  if(route === "logout" || route === "logoutAndRefresh") sendedObject["token"] = userCredentials.refreshToken;
+  if(route === "auth/login" || route === "auth/check-credential") sendedObject["password"] = userCredentials.password;
+  if(route === "auth/refresh-token") sendedObject["refreshToken"] = userCredentials.refreshToken;
+  if(route === "auth/logout" || route === "auth/logoutAndRefresh") sendedObject["token"] = userCredentials.refreshToken;
 
   return await routeRequest({route, sendedObject, accessToken, restType: "post"});
 };
