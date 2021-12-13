3d4695679509f892ccd650b83ee13f2eb74567a3
SHergibo <sachau888@hotmail.com>
2021-12-13 15:45:52 +0100

    fix: Fixed route param for basicRouteAuth and createOneUserAndLogin function in multiple tests in auth-logout.test.js.

--- tests/auth-test/auth-logout.test.js
+++ tests/auth-test/auth-logout.test.js
@@ -8,35 +8,35 @@ dbManagement();
 
 describe("Test logout auth controller", () => {
   it("Test 1) logout with no email", async () => {
-    const {responseLogin} = await createOneUserAndLogin({ route : "login" });
+    const {responseLogin} = await createOneUserAndLogin({ route : "auth/login" });
 
     const userCredentialsLogout = {
       email: null,
       refreshToken: responseLogin.body.token.refreshToken.token
     };
 
-    const responseLogout = await basicRouteAuth({userCredentials: userCredentialsLogout, route: "logout", accessToken : responseLogin.body.token.accessToken});
+    const responseLogout = await basicRouteAuth({userCredentials: userCredentialsLogout, route: "auth/logout", accessToken : responseLogin.body.token.accessToken});
 
     expect(responseLogout.body.output.statusCode).toBe(400);
     expect(responseLogout.body.isBoom).toBe(true);
     expect(responseLogout.body.output.payload.message).toMatch("An email or a token is required to logout !");
   });
   it("Test 2) logout with no token", async () => {
-    const {adminOne, responseLogin} = await createOneUserAndLogin({ route : "login" });
+    const {adminOne, responseLogin} = await createOneUserAndLogin({ route : "auth/login" });
 
     const userCredentialsLogout = {
       email: adminOne.email,
       refreshToken: null
     };
 
-    const responseLogout = await basicRouteAuth({userCredentials: userCredentialsLogout, route: "logout", accessToken : responseLogin.body.token.accessToken});
+    const responseLogout = await basicRouteAuth({userCredentials: userCredentialsLogout, route: "auth/logout", accessToken : responseLogin.body.token.accessToken});
 
     expect(responseLogout.body.output.statusCode).toBe(400);
     expect(responseLogout.body.isBoom).toBe(true);
     expect(responseLogout.body.output.payload.message).toMatch("An email or a token is required to logout !");
   });
   it("Test 3) logout", async () => {
-    const {adminOne, responseLogin, objectClientSocket} = await createOneUserAndLogin({ withSocket : true, route : "login" });
+    const {adminOne, responseLogin, objectClientSocket} = await createOneUserAndLogin({ withSocket : true, route : "auth/login" });
 
     const userCredentialsLogout = {
       email: adminOne.email,
@@ -48,7 +48,7 @@ describe("Test logout auth controller", () => {
       logoutSameNavigator = true;
     });
 
-    const responseLogout = await basicRouteAuth({userCredentials: userCredentialsLogout, route: "logout", accessToken : responseLogin.body.token.accessToken});
+    const responseLogout = await basicRouteAuth({userCredentials: userCredentialsLogout, route: "auth/logout", accessToken : responseLogin.body.token.accessToken});
 
     // expect(logoutSameNavigator).toBe(true);
     expect(responseLogout.statusCode).toBe(204);
