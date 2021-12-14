907c9a0945ee15907345fdca53ec8f0c29288461
SHergibo <sachau888@hotmail.com>
2021-12-14 15:02:20 +0100

    feat: Created test 1 for findOne-option.test.js.

---
+++ tests/option-test/findOne-option.test.js
@@ -0,0 +1,28 @@
+const { createOneUserAndLogin } = require('../global-helper/createOneUserAndLogin.helper'),
+      { routeRequest } = require('../global-helper/routeRequest.helper'),
+      Option = require('../../api/models/option.model');
+
+const { dbManagement } = require('../db-management-utils');
+
+dbManagement();
+
+describe("Test find one option", () => {
+  it("Test 1) test find one option", async () => {
+    const {adminOne, responseLogin} = await createOneUserAndLogin({ route : "auth/login" });
+
+    const response = await routeRequest({route: `options/${adminOne._id}`, restType : "get", accessToken : responseLogin.body.token.accessToken});
+
+    const userOptions = await Option.findOne({userId : adminOne._id});
+
+    for (const key in response.body) {
+      if(key === "_id" || key === "userId"){
+        expect(response.body[key].toString()).toBe(userOptions[key].toString());
+      } else if(key === "dateMailGlobal" || key === "dateMailShoppingList" || key === "warningExpirationDate") {
+        expect(response.body[key].value).toBe(userOptions[key].value);
+        expect(response.body[key].label).toBe(userOptions[key].label);
+      } else {
+        expect(response.body[key]).toBe(userOptions[key]);
+      }
+    }
+  });
+});
