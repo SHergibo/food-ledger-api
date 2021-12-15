37be11fc2911ea8b3a714ab01200c563fc84734d
SHergibo <sachau888@hotmail.com>
2021-12-15 14:50:02 +0100

    feat: Added test 1 for remove-brand.test.js.

---
+++ tests/brand-test/remove-brand.test.js
@@ -0,0 +1,44 @@
+const { createOneUserAndLogin } = require('../global-helper/createOneUserAndLogin.helper'),
+      { createBrands } = require('./brand-helper/createBrands.helper'),
+      { routeRequest } = require('../global-helper/routeRequest.helper'),
+      Brand = require('../../api/models/brand.model');
+
+const { dbManagement } = require('../db-management-utils');
+const { disconnectSocketClient } = require('../socket-io-management-utils');
+
+dbManagement();
+
+describe("Test remove one brand", () => {
+  it("Test 1) remove one brand with brandId", async () => {
+    const {adminOne, responseLogin, objectClientSocket} = await createOneUserAndLogin({withSocket: true, route : "auth/login" });
+    const brandsArray = await createBrands({householdId : adminOne.householdId});
+
+    let socketToBrand;
+    objectClientSocket.clientSocketAdminOne.on("updateDataArray", (data) => {
+      socketToBrand = data;
+    });
+
+    const response = await routeRequest({route: `brands/${brandsArray[0]._id}`, restType : "delete", accessToken : responseLogin.body.token.accessToken});
+
+    const deletedBrand = await Brand.findById(brandsArray[0]._id);
+
+    expect(response.statusCode).toBe(204);
+    expect(deletedBrand).toBeNull();
+
+    brandsArray.shift();
+    for (const [index, data] of socketToBrand.arrayData.entries()) {
+      for (const key in data) {
+        if(key === "_id"){
+          expect(data[key].toString()).toBe(brandsArray[index][key].toString());
+        } else if(key === "brandName") {
+          expect(data[key].value).toBe(brandsArray[index][key].value);
+          expect(data[key].label).toBe(brandsArray[index][key].label);
+        } else if(key !== "isBeingEdited") {
+          expect(data[key]).toBe(brandsArray[index][key]);
+        }
+      }
+    }
+
+    disconnectSocketClient(objectClientSocket);
+  });
+});
