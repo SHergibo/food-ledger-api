5d1715324089ff9b7084dfc16a1d9240c2f5a962
SHergibo <sachau888@hotmail.com>
2021-12-15 14:36:32 +0100

    feat: Created test 1 for update-brand.test.js.

---
+++ tests/brand-test/update-brand.test.js
@@ -0,0 +1,47 @@
+const { createOneUserAndLogin } = require('../global-helper/createOneUserAndLogin.helper'),
+      { createBrands } = require('./brand-helper/createBrands.helper'),
+      { routeRequest } = require('../global-helper/routeRequest.helper');
+
+const { dbManagement } = require('../db-management-utils');
+const { disconnectSocketClient } = require('../socket-io-management-utils');
+
+dbManagement();
+
+
+describe("Test update brand", () => {
+  it("Test 1) test update brand", async () => {
+    const {adminOne, responseLogin, objectClientSocket} = await createOneUserAndLogin({withSocket: true, route : "auth/login" });
+    const brandsArray = await createBrands({householdId : adminOne.householdId});
+
+    const updatedBrand = {
+      brandName: {
+        label: `brandUpdated-0`,
+        value: `brandUpdated-0`
+      },
+      numberOfProduct: 0,
+      numberOfHistoric: 1,
+    };
+
+    let socketToBrand;
+    objectClientSocket.clientSocketAdminOne.on("updatedData", (data) => {
+      socketToBrand = data;
+    });
+
+    const response = await routeRequest({route: `brands/${brandsArray[0]._id}`, restType : "patch", sendedObject: updatedBrand, accessToken : responseLogin.body.token.accessToken});
+
+    for (const key in response.body) {
+      if(key === "brandName") {
+        expect(response.body[key].value).toBe(updatedBrand[key].value);
+        expect(response.body[key].label).toBe(updatedBrand[key].label);
+
+        expect(response.body[key].value).toBe(socketToBrand[key].value);
+        expect(response.body[key].label).toBe(socketToBrand[key].label);
+      } else if(key !== "_id" && key !== "isBeingEdited") {
+        expect(response.body[key]).toBe(updatedBrand[key]);
+        expect(response.body[key]).toBe(socketToBrand[key]);
+      }
+    }
+
+    disconnectSocketClient(objectClientSocket);
+  });
+});
