a9b5be2dfee21eb7e8290172f14b125ca15009d3
SHergibo <sachau888@hotmail.com>
2021-12-16 15:39:39 +0100

    feat: Added test 1 for sendMail-shoppingList.test.js.

---
+++ tests/shoppingList-test/sendMail-shoppingList.test.js
@@ -0,0 +1,20 @@
+const { createOneUserAndLogin } = require('../global-helper/createOneUserAndLogin.helper'),
+      { createShoppingList } = require('./shoppingList-helper/createShoppingList.helper'),
+      { routeRequest } = require('../global-helper/routeRequest.helper');
+
+const { dbManagement } = require('../db-management-utils');
+
+dbManagement();
+
+describe("Test send shopping list email", () => {
+  it("Test 1) test send shopping list email", async () => {
+    const {adminOne, responseLogin} = await createOneUserAndLogin({ route : "auth/login" });
+    await createShoppingList({householdId : adminOne.householdId});
+
+    /* Comment everything after this line to not be spammed with testing emails */
+
+    // const response = await routeRequest({route: `shopping-lists/send-mail/${adminOne.householdId}`, restType : "get", accessToken : responseLogin.body.token.accessToken});
+
+    // expect(response.statusCode).toBe(204);
+  });
+});
