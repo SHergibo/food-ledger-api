7e5b2a6633e313b21786634e58ed20d6de2515d4
SHergibo <sachau888@hotmail.com>
2021-12-17 14:35:38 +0100

    fix: Deleted token-auth.route.js.

--- api/routes/v1/token-auth.route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router(),
-      TokenAuthController = require(`${process.cwd()}/api/controllers/token-auth.controller`);
- 
-router
-  .route('/:tokenId')
-    .post(TokenAuthController.createNewToken)
-    .patch(TokenAuthController.updateUsedToken);
-
-module.exports = router;
