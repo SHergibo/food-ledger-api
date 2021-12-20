c317c0ef59b579d1b8d731cacf3a1b332ba51033
SHergibo <sachau888@hotmail.com>
2021-12-20 16:54:18 +0100

    fix: Added dynamic week of the year when testing chartFour in test 1 of getChartData-statistic.test.js.

--- tests/statistic-test/getChartData-statistic.test.js
+++ tests/statistic-test/getChartData-statistic.test.js
@@ -1,6 +1,7 @@
 const { createOneUserAndLogin } = require('../global-helper/createUserAndLogin.helper'),
       { createProduct } = require('./statistic-helper/createProduct.helper'),
-      { routeRequest } = require('../global-helper/routeRequest.helper');
+      { routeRequest } = require('../global-helper/routeRequest.helper'),
+      Moment = require('moment');
 
 const { dbManagement } = require('../db-management-utils');
 
@@ -16,7 +17,9 @@ describe("Test get chart data", () => {
     expect(response.body.statistics.chartOne["2021"][11]).toBe(1);
     expect(response.body.statistics.chartTwo[0]).toBe(1);
     expect(response.body.statistics.chartThree[0]).toBe(createProductData.kcal);
-    expect(response.body.statistics.chartFour["2021"][49]).toBe(1);
+
+    let currentWeek = Moment(new Date(), "MMDDYYYY").isoWeek() - 1;
+    expect(response.body.statistics.chartFour["2021"][currentWeek]).toBe(1);
 
   });
 });
