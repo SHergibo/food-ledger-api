34075fc8a22ab74d933f1b8f10cda6547a2e7107
SHergibo <sachau888@hotmail.com>
2021-12-21 16:22:11 +0100

    feat: Added tests 2 and 4 in findPaginateSendedNotif-notification.test.js.

--- tests/notification-test/findPaginateSendedNotif-notification.test.js
+++ tests/notification-test/findPaginateSendedNotif-notification.test.js
@@ -6,8 +6,8 @@ const { dbManagement } = require('../db-management-utils');
 
 dbManagement();
 
-describe("Test find paginate received notification", () => {
-  it("Test 1) test find paginate received notification for adminOne", async () => {
+describe("Test find paginate sended notification", () => {
+  it("Test 1) test find paginate sended notification for adminOne", async () => {
     const {adminOne, userOne, adminTwo, userTwo} = await createFourUsersAndLogin({ route : "auth/login" });
     const notificationObject = await createNotification({
       adminOne : adminOne.userData, 
@@ -31,7 +31,33 @@ describe("Test find paginate received notification", () => {
     expect(response.body.totalNotifSended).toBe(notificationObject.adminOne.notifSended.length);
 
   });
-  it("Test 2) test find paginate received notification for userOne", async () => {
+
+  it("Test 2) test find paginate sended notification for adminOne with page query to 1", async () => {
+    const {adminOne, userOne, adminTwo, userTwo} = await createFourUsersAndLogin({ route : "auth/login" });
+    const notificationObject = await createNotification({
+      adminOne : adminOne.userData, 
+      userOne : userOne.userData, 
+      adminTwo : adminTwo.userData, 
+      userTwo: userTwo.userData 
+    });
+
+    const response = await routeRequest({route: `notifications/pagination-sended-notification/${adminOne.userData._id}?page=1`, restType : "get", accessToken : adminOne.responseLogin.body.token.accessToken});
+
+    for (const [index, data] of response.body.arrayData.entries()) {
+      for (const key in data) {
+        let indexNotif = notificationObject.adminOne.notifSended.length - response.body.arrayData.length;
+        if(key === "_id"){
+          expect(data[key].toString()).toBe(notificationObject.adminOne.notifSended[indexNotif + index][key].toString());
+        } else if(key !== "userId") {
+          expect(data[key]).toBe(notificationObject.adminOne.notifSended[indexNotif + index][key]);
+        }
+      }
+    }
+
+    expect(response.body.totalNotifSended).toBe(notificationObject.adminOne.notifSended.length);
+
+  });
+  it("Test 3) test find paginate sended notification for userOne", async () => {
     const {adminOne, userOne, adminTwo, userTwo} = await createFourUsersAndLogin({ route : "auth/login" });
     const notificationObject = await createNotification({
       adminOne : adminOne.userData, 
@@ -48,6 +74,31 @@ describe("Test find paginate received notification", () => {
           expect(data[key].toString()).toBe(notificationObject.userOne.notifSended[index][key].toString());
         } else if(key !== "userId") {
           expect(data[key]).toBe(notificationObject.userOne.notifSended[index][key]);
+        }
+      }
+    }
+
+    expect(response.body.totalNotifSended).toBe(notificationObject.userOne.notifSended.length);
+
+  });
+  it("Test 4) test find paginate sended notification for userOne with page query to 1", async () => {
+    const {adminOne, userOne, adminTwo, userTwo} = await createFourUsersAndLogin({ route : "auth/login" });
+    const notificationObject = await createNotification({
+      adminOne : adminOne.userData, 
+      userOne : userOne.userData, 
+      adminTwo : adminTwo.userData, 
+      userTwo: userTwo.userData 
+    });
+
+    const response = await routeRequest({route: `notifications/pagination-sended-notification/${userOne.userData._id}?page=1`, restType : "get", accessToken : userOne.responseLogin.body.token.accessToken});
+
+    for (const [index, data] of response.body.arrayData.entries()) {
+      for (const key in data) {
+        let indexNotif = notificationObject.userOne.notifSended.length - response.body.arrayData.length;
+        if(key === "_id"){
+          expect(data[key].toString()).toBe(notificationObject.userOne.notifSended[indexNotif + index][key].toString());
+        } else if(key !== "userId") {
+          expect(data[key]).toBe(notificationObject.userOne.notifSended[indexNotif + index][key]);
         }
       }
     }
