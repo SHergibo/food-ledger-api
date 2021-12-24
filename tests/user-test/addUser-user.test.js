52f5dc7053eae3a56bb02cea5999497dbbc8741e
SHergibo <sachau888@hotmail.com>
2021-12-24 14:54:32 +0100

    feat: Added test 3 in addUser-user.test.js

--- tests/user-test/addUser-user.test.js
+++ tests/user-test/addUser-user.test.js
@@ -2,6 +2,8 @@ const request = require("supertest"),
       app = require("./../../config/app.config"),
       { api } = require('./../../config/environment.config'),
       Household = require('./../../api/models/household.model'),
+      Notification = require('./../../api/models/notification.model'),
+      Moment = require('moment-timezone'),
       { adminOneDataComplete, userDataMissing, userTwoDataComplete, userFourDataComplete } = require('./../test-data'),
       { createUser } = require('../global-helper/createUserManagement.helper'),
       Client = require("socket.io-client");
@@ -31,15 +33,31 @@ describe("Add user", () => {
     expect(household.userId.toString()).toBe(response.body._id.toString());
   });
   it("3) Add user with household name and bad other member codes (usercode doesn't exist and member is in isWaiting familly)", async () => {
-    // const response = await request(app)
-    // .post(`/api/${api}/users`)
-    // .send(adminOneDataComplete);
+    let userOne = await createUser({userData : userTwoDataComplete});
 
-    // const household = await Household.findById(response.body.householdId);
-    // expect(response.statusCode).toBe(200);
-    // expect(response.body.householdId.toString()).toBe(household._id.toString());
-    // expect(response.body.role).toBe("admin");
-    // expect(household.userId.toString()).toBe(response.body._id.toString());
+    let newNotification = await new Notification({
+      message: "Vous avez été désigné.e comme nouvel.le administrateur.trice de cette famille par l'ancien.ne administrateur.trice, acceptez-vous cette requête ou passez l'administration à un.e autre membre de votre famille. Attention si vous êtes le/la dernier.ère membre éligible de cette famille, la famille sera supprimée et ne pourra pas être récupérée!",
+      householdId: "606dad080ac1c22766b37a53",
+      userId: userOne._id,
+      type: "request-delegate-admin",
+      urlRequest: "delegate-admin",
+      expirationDate: Moment().add({h: 23, m: 59, s: 59}).toDate()
+    });
+    await newNotification.save();
+
+    adminOneDataComplete.othermember = [userOne.usercode, "ksdjfhsjfkds"];
+
+    const response = await request(app)
+    .post(`/api/${api}/users`)
+    .send(adminOneDataComplete);
+
+    expect(response.body.data[0].usercode).toBe(userOne.usercode);
+    expect(response.body.data[0].errorType).toBe("userIsWaiting");
+    expect(response.body.data[1].usercode).toBe("ksdjfhsjfkds");
+    expect(response.body.data[1].errorType).toBe("userCodeNotFound");
+    expect(response.body.output.statusCode).toBe(400);
+    expect(JSON.parse(response.error.text).isBoom).toBe(true);
+    expect(response.body.output.payload.message).toBe("Il y a un ou plusieurs problèmes avec certains de vos codes utilisateurs!");
   });
   it("4) Add user with household name and good other member codes", async () => {
     let clientSocketAdminOne = Client(`http://localhost:8003`);
