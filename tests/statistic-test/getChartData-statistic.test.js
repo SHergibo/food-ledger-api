8d7de1b501e159367b2f2b18a553ae344f34264a
SHergibo <sachau888@hotmail.com>
2022-01-28 14:19:26 +0100

    fix: Fixed bug for the last expect in test 1 in getChartData-statistic.test.js.

--- tests/statistic-test/getChartData-statistic.test.js
+++ tests/statistic-test/getChartData-statistic.test.js
@@ -1,25 +1,37 @@
-const { createOneUserAndLogin } = require('../global-helper/createUserAndLogin.helper'),
-      { createProduct } = require('./statistic-helper/createProduct.helper'),
-      { routeRequest } = require('../global-helper/routeRequest.helper'),
-      Moment = require('moment');
+const {
+    createOneUserAndLogin,
+  } = require("../global-helper/createUserAndLogin.helper"),
+  { createProduct } = require("./statistic-helper/createProduct.helper"),
+  { routeRequest } = require("../global-helper/routeRequest.helper"),
+  Moment = require("moment");
 
-const { dbManagement } = require('../db-management-utils');
+const { dbManagement } = require("../db-management-utils");
 
 dbManagement();
 
 describe("Test get chart data", () => {
   it("Test 1) test get chart data", async () => {
-    const {adminOne, responseLogin} = await createOneUserAndLogin({ route : "auth/login" });
-    const createProductData = await createProduct({householdId : adminOne.householdId});
+    const { adminOne, responseLogin } = await createOneUserAndLogin({
+      route: "auth/login",
+    });
+    const createProductData = await createProduct({
+      householdId: adminOne.householdId,
+    });
 
-    const response = await routeRequest({route: `statistics/chart-data/${adminOne.householdId}`, restType : "get", accessToken : responseLogin.body.token.accessToken});
+    const response = await routeRequest({
+      route: `statistics/chart-data/${adminOne.householdId}`,
+      restType: "get",
+      accessToken: responseLogin.body.token.accessToken,
+    });
 
     expect(response.body.statistics.chartOne["2021"][11]).toBe(1);
     expect(response.body.statistics.chartTwo[0]).toBe(1);
     expect(response.body.statistics.chartThree[0]).toBe(createProductData.kcal);
 
     let currentWeek = Moment(new Date(), "MMDDYYYY").isoWeek() - 1;
-    expect(response.body.statistics.chartFour["2021"][currentWeek]).toBe(1);
-
+    let currentYear = Moment(new Date(), "MMDDYYYY").year();
+    expect(response.body.statistics.chartFour[currentYear][currentWeek]).toBe(
+      1
+    );
   });
 });
