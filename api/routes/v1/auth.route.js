5fdbd28d8a33f5084cb0fe1166ec9914ba3d5560
SHergibo <sachau888@hotmail.com>
2022-01-31 15:32:21 +0100

    fix: Changed logoutAndRefresh route name to logout-and-refresh in auth.route.js.

--- api/routes/v1/auth.route.js
+++ api/routes/v1/auth.route.js
@@ -1,30 +1,29 @@
-const router = require('express').Router(),
-      AuthController = require(`${process.cwd()}/api/controllers/auth.controller`),
-      { authorize, ADMIN, LOGGED_USER } = require('../../middlewares/auth.middleware');
+const router = require("express").Router(),
+  AuthController = require(`${process.cwd()}/api/controllers/auth.controller`),
+  {
+    authorize,
+    ADMIN,
+    LOGGED_USER,
+  } = require("../../middlewares/auth.middleware");
+
+router.route("/login").post(AuthController.login);
+
+router.route("/check-credential").post(AuthController.checkCredential);
+
+router.route("/refresh-token").post(AuthController.refresh);
 
 router
-  .route('/login')
-    .post(AuthController.login);
-  
-router
-  .route('/check-credential')
-    .post(AuthController.checkCredential);
+  .route("/check-token")
+  .get(authorize([ADMIN, LOGGED_USER]), (req, res, next) => {
+    return res.status(200).send();
+  });
 
 router
-  .route('/refresh-token')
-    .post(AuthController.refresh);
+  .route("/logout")
+  .post(authorize([ADMIN, LOGGED_USER]), AuthController.logout);
 
 router
-  .route('/check-token')
-    .get(authorize([ADMIN, LOGGED_USER]), (req, res, next) => {return res.status(200).send()});
-  
-
-router
-  .route('/logout')
-    .post(authorize([ADMIN, LOGGED_USER]), AuthController.logout);
-
-router
-  .route('/logoutAndRefresh')
-    .post(authorize([ADMIN, LOGGED_USER]), AuthController.logoutAndRefresh);
+  .route("/logout-and-Refresh")
+  .post(authorize([ADMIN, LOGGED_USER]), AuthController.logoutAndRefresh);
 
 module.exports = router;
