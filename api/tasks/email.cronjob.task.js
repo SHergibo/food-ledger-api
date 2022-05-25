0537ba968702ac84cde9b915dc6671d47887087f
SHergibo <sachau888@hotmail.com>
2022-05-25 14:36:02 +0200

    feat: Used sendGrid email instead of nodemailer to send email in email.cronjob.task.js.

--- api/tasks/email.cronjob.task.js
+++ api/tasks/email.cronjob.task.js
@@ -1,56 +1,57 @@
-const HouseHold = require('./../models/household.model'),
-      Option = require('./../models/option.model'),
-      ShoppingList = require('./../models/shopping-list.model'),
-      Product = require('./../models/product.model'),
-      Moment = require('moment'),
-      NodeMailer = require('./../../api/helpers/nodemailer.helper'),
-      { loggerError } = require('./../../config/logger.config');
+const HouseHold = require("./../models/household.model"),
+  Option = require("./../models/option.model"),
+  ShoppingList = require("./../models/shopping-list.model"),
+  Product = require("./../models/product.model"),
+  Moment = require("moment"),
+  { sendGridEmail } = require("./../helpers/sendGrid-mail.helper"),
+  { loggerError } = require("./../../config/logger.config");
 
-const findShoppingListAndMailIt = async (householdId) => {
+const findShoppingListAndMailIt = async (householdId, userId) => {
   try {
-    let shoppingList = await ShoppingList.find({householdId : householdId})
-    .populate({
-      path: 'product',
-      populate : {
-        path: 'brand',
-        select: {brandName: 1}
-      },
-      select: {
-        name: 1,
-        weight: 1,
-      }
-    })
-    .populate({
-      path: 'historic',
-      populate : {
-        path: 'brand',
-        select: {brandName: 1}
-      },
-      select: {
-        name: 1,
-        weight: 1,
-      }
-    });
-    if(shoppingList.length >= 1){
-  
-      let tr = shoppingList.map(shopping => {
-        if(shopping.product){
-          return `<tr>
+    let shoppingList = await ShoppingList.find({ householdId: householdId })
+      .populate({
+        path: "product",
+        populate: {
+          path: "brand",
+          select: { brandName: 1 },
+        },
+        select: {
+          name: 1,
+          weight: 1,
+        },
+      })
+      .populate({
+        path: "historic",
+        populate: {
+          path: "brand",
+          select: { brandName: 1 },
+        },
+        select: {
+          name: 1,
+          weight: 1,
+        },
+      });
+    if (shoppingList.length >= 1) {
+      let tr = shoppingList
+        .map((shopping) => {
+          if (shopping.product) {
+            return `<tr>
                     <td>${shopping.product.name}</td>
                     <td>${shopping.product.brand.brandName.label}</td>
                     <td>${shopping.product.weight}gr</td>
                     <td>${shopping.numberProduct}</td>
                   </tr>`;
-        }else if (shopping.historic){
-          return `<tr>
+          } else if (shopping.historic) {
+            return `<tr>
                     <td>${shopping.historic.name}</td>
                     <td>${shopping.historic.brand.brandName.label}</td>
                     <td>${shopping.historic.weight}gr</td>
                     <td>${shopping.numberProduct}</td>
                   </tr>`;
-        }
-      }).join('');
-  
+          }
+        })
+        .join("");
+
       let output = `<h2>Voici votre liste de course à faire pour votre stock :</h2>
         <table>
           <thead>
@@ -66,12 +67,22 @@ const findShoppingListAndMailIt = async (householdId) => {
           </tbody>
       </table>
       `;
-  
-      NodeMailer.send(output, 'Votre liste de course pour votre stock!');
+
+      const dynamic_template_data = {
+        subject: "Votre liste de course pour votre stock !",
+        title: "Votre liste de course pour votre stock !",
+        output: output,
+      };
+
+      const user = await User.findById(userId);
+      await sendGridEmail({
+        to: user.email,
+        templateType: "list",
+        dynamic_template_data,
+      });
     }
   } catch (error) {
     loggerError.error(error);
-    NodeMailer.send(error, 'Une erreur est survenue dans la fonction findShoppingListAndMailIt!');
   }
 };
 
@@ -79,101 +90,110 @@ exports.shoppingListEmail = async () => {
   try {
     let today = new Date();
     let households = await HouseHold.find({});
-    households.forEach(household => {
+    households.forEach((household) => {
       let members = household.members;
       members.forEach(async (member) => {
-        let option = await Option.findOne({userId : member.userData});
-        if(option.sendMailShoppingList){
+        let option = await Option.findOne({ userId: member.userData });
+        if (option.sendMailShoppingList) {
           let numberWeek = Moment(today, "DDMMYYYY").isoWeek();
           switch (option.dateMailShoppingList.value) {
             case 0:
-              findShoppingListAndMailIt(household._id);
+              findShoppingListAndMailIt(household._id, member.userData);
               break;
             case 1:
-              if(numberWeek % 2 === 0){
-                findShoppingListAndMailIt(household._id);
+              if (numberWeek % 2 === 0) {
+                findShoppingListAndMailIt(household._id, member.userData);
               }
               break;
             case 2:
-              if(numberWeek % 2 !== 0){
-                findShoppingListAndMailIt(household._id);
+              if (numberWeek % 2 !== 0) {
+                findShoppingListAndMailIt(household._id, member.userData);
               }
               break;
-          
+
             default:
               break;
           }
-        }else{
+        } else {
           return;
         }
       });
     });
   } catch (error) {
     loggerError.error(error);
-    NodeMailer.send(error, 'Une erreur est survenue dans la fonction shoppingListEmail!');
   }
 };
 
-const findProductAndMailIt = async (householdId, warningExpirationDate) => {
+const findProductAndMailIt = async (
+  householdId,
+  userId,
+  warningExpirationDate
+) => {
   try {
     let month = new Date().getMonth() + 1;
     let year = new Date().getFullYear();
-    let products = await Product.find({householdId : householdId}).populate('brand', "brandName");
+    let products = await Product.find({ householdId: householdId }).populate(
+      "brand",
+      "brandName"
+    );
     let productExpirationDate = [];
     let warningDate;
-    
+
     switch (warningExpirationDate) {
       case 0:
-        if(month === 12){
+        if (month === 12) {
           warningDate = new Date(`01/01/${year + 1}`);
-        }else{
+        } else {
           warningDate = new Date(`${month + 1}/01/${year}`);
         }
         break;
       case 1:
-        if(month === 11){
+        if (month === 11) {
           warningDate = new Date(`02/01/${year + 1}`);
-        }else if (month === 12){
+        } else if (month === 12) {
           warningDate = new Date(`03/01/${year + 1}`);
-        }else{
+        } else {
           warningDate = new Date(`${month + 2}/01/${year}`);
         }
         break;
       case 2:
-        if(month === 11){
+        if (month === 11) {
           warningDate = new Date(`03/01/${year + 1}`);
-        }else if (month === 12){
+        } else if (month === 12) {
           warningDate = new Date(`04/01/${year + 1}`);
-        }else{
+        } else {
           warningDate = new Date(`${month + 3}/01/${year}`);
         }
         break;
-    
+
       default:
         break;
     }
 
-    if(products.length >= 1){
-
-      await products.forEach(product => {
-        product.expirationDate.forEach(date => {
-          if(date.expDate < warningDate ){
-            let searchProduct = productExpirationDate.find(productInArray => productInArray.id === product._id)
-            if(searchProduct){
+    if (products.length >= 1) {
+      await products.forEach((product) => {
+        product.expirationDate.forEach((date) => {
+          if (date.expDate < warningDate) {
+            let searchProduct = productExpirationDate.find(
+              (productInArray) => productInArray.id === product._id
+            );
+            if (searchProduct) {
               searchProduct.expirationDate.push({
                 expDate: date.expDate,
-                number: date.productLinkedToExpDate
+                number: date.productLinkedToExpDate,
               });
-            }else{
+            } else {
               let productObject = {
                 id: product._id,
                 name: product.name,
                 brand: product.brand.brandName.label,
                 location: product.location,
-                expirationDate: [{
-                  expDate: date.expDate,
-                  number: date.productLinkedToExpDate
-                }]
+                expirationDate: [
+                  {
+                    expDate: date.expDate,
+                    number: date.productLinkedToExpDate,
+                  },
+                ],
               };
               productExpirationDate = [...productExpirationDate, productObject];
             }
@@ -181,22 +201,28 @@ const findProductAndMailIt = async (householdId, warningExpirationDate) => {
         });
       });
 
-      if(productExpirationDate.length >= 1){    
-        let tr = productExpirationDate.map(product => {
-        let numberProduct = 0;
-        let listDate = product.expirationDate.map(expDateObject => {
-          numberProduct += expDateObject.number;
-          return `<li>${Moment(expDateObject.expDate).format("DD-MM-YYYY")} - nombre ${expDateObject.number}</li>`;
-        }).join('');
-          return `<tr>
+      if (productExpirationDate.length >= 1) {
+        let tr = productExpirationDate
+          .map((product) => {
+            let numberProduct = 0;
+            let listDate = product.expirationDate
+              .map((expDateObject) => {
+                numberProduct += expDateObject.number;
+                return `<li>${Moment(expDateObject.expDate).format(
+                  "DD-MM-YYYY"
+                )} - nombre ${expDateObject.number}</li>`;
+              })
+              .join("");
+            return `<tr>
                     <td>${product.name}</td>
                     <td>${product.brand}</td>
                     <td>${product.location}</td>
                     <td>x ${numberProduct}</td>
                     <td><ul>${listDate}</ul></td>
                   </tr>`;
-        }).join('');
-    
+          })
+          .join("");
+
         let output = `<h2>Voici la liste des produits de votre stock bientôt périmés :</h2>
           <table>
             <thead>
@@ -214,12 +240,23 @@ const findProductAndMailIt = async (householdId, warningExpirationDate) => {
         </table>
         `;
 
-        NodeMailer.send(output, 'Vous avez des produits proches de leur date de péremptions!');
+        const dynamic_template_data = {
+          subject:
+            "Vous avez des produits proches de leur date de péremptions!",
+          title: "Vous avez des produits proches de leur date de péremptions!",
+          output: output,
+        };
+
+        const user = await User.findById(userId);
+        await sendGridEmail({
+          to: user.email,
+          templateType: "list",
+          dynamic_template_data,
+        });
       }
     }
   } catch (error) {
     loggerError.error(error);
-    NodeMailer.send(error, 'Une erreur est survenue dans la fonction findProductAndMailIt!');
   }
 };
 
@@ -229,45 +266,55 @@ exports.globalEmail = async () => {
     let sendEveryTwoMonth = [1, 3, 5, 7, 9, 11];
     let sendEveryTreeMonth = [0, 3, 6, 9];
     let households = await HouseHold.find({});
-  
-    households.forEach(household => {
+
+    households.forEach((household) => {
       let members = household.members;
       members.forEach(async (member) => {
-        let option = await Option.findOne({userId : member.userData});
+        let option = await Option.findOne({ userId: member.userData });
 
-        if(!option.sendMailGlobal && !option.sendMailShoppingList) return;
+        if (!option.sendMailGlobal && !option.sendMailShoppingList) return;
 
-        if(option.sendMailGlobal){
+        if (option.sendMailGlobal) {
           switch (option.dateMailGlobal.value) {
             case 0:
-              findProductAndMailIt(household._id, option.warningExpirationDate.value);
+              findProductAndMailIt(
+                household._id,
+                member.userData,
+                option.warningExpirationDate.value
+              );
               break;
             case 1:
-              if(sendEveryTwoMonth.includes(today.getMonth())){
-                findProductAndMailIt(household._id, option.warningExpirationDate.value);
+              if (sendEveryTwoMonth.includes(today.getMonth())) {
+                findProductAndMailIt(
+                  household._id,
+                  member.userData,
+                  option.warningExpirationDate.value
+                );
               }
               break;
             case 2:
-              if(sendEveryTreeMonth.includes(today.getMonth())){
-                findProductAndMailIt(household._id, option.warningExpirationDate.value);
+              if (sendEveryTreeMonth.includes(today.getMonth())) {
+                findProductAndMailIt(
+                  household._id,
+                  member.userData,
+                  option.warningExpirationDate.value
+                );
               }
               break;
-          
+
             default:
               break;
           }
         }
 
-        if(option.sendMailShoppingList){
-          if(option.dateMailShoppingList.value === 3){
-            findShoppingListAndMailIt(household._id);
+        if (option.sendMailShoppingList) {
+          if (option.dateMailShoppingList.value === 3) {
+            findShoppingListAndMailIt(household._id, member.userData);
           }
         }
       });
     });
-    
   } catch (error) {
     loggerError.error(error);
-    NodeMailer.send(error, 'Une erreur est survenue dans la fonction globalEmail!');
   }
 };
