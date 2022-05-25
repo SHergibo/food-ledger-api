ed31f43551885590a8a9ede4ccc26d3c4adf1719
SHergibo <sachau888@hotmail.com>
2022-05-25 14:38:45 +0200

    fix: Deleted nodemailer in statistic.cronjob.task.js.

--- api/tasks/statistic.cronjob.task.js
+++ api/tasks/statistic.cronjob.task.js
@@ -1,9 +1,8 @@
-const HouseHold = require('./../models/household.model'),
-      Product = require('./../models/product.model'),
-      Statistic = require('./../models/statistic.model'),
-      Moment = require('moment'),
-      NodeMailer = require('./../../api/helpers/nodemailer.helper'),
-      { loggerError } = require('./../../config/logger.config');
+const HouseHold = require("./../models/household.model"),
+  Product = require("./../models/product.model"),
+  Statistic = require("./../models/statistic.model"),
+  Moment = require("moment"),
+  { loggerError } = require("./../../config/logger.config");
 
 exports.statisticChartFour = async () => {
   try {
@@ -12,46 +11,47 @@ exports.statisticChartFour = async () => {
     households.forEach(async (household) => {
       let products = await Product.find({ householdId: household._id });
       let statistic = await Statistic.findOne({ householdId: household._id });
-      if(products.length >= 1){
+      if (products.length >= 1) {
         let totalProduct = 0;
-        products.forEach(product => {
+        products.forEach((product) => {
           totalProduct = totalProduct + product.number;
         });
 
         let getYear = new Date().getFullYear();
         let currentYear;
-        if(!statistic){
+        if (!statistic) {
           currentYear = [...Array(Moment().isoWeeksInYear()).fill(0)];
-          currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] = totalProduct;
+          currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] =
+            totalProduct;
           dataChartFour[getYear] = currentYear;
 
           let stat = {
-            statistics : {
+            statistics: {
               chartFour: dataChartFour,
             },
-            householdId: household._id
-          }
-      
+            householdId: household._id,
+          };
+
           statistic = new Statistic(stat);
           await statistic.save();
-
-        }else{
+        } else {
           dataChartFour = statistic.statistics.chartFour;
-          if(dataChartFour[getYear]){
-            currentYear = dataChartFour[getYear]
-          }else{
+          if (dataChartFour[getYear]) {
+            currentYear = dataChartFour[getYear];
+          } else {
             currentYear = [...Array(Moment().isoWeeksInYear()).fill(0)];
           }
-          currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] = totalProduct;
+          currentYear[Moment(new Date(), "MMDDYYYY").isoWeek() - 1] =
+            totalProduct;
           dataChartFour[getYear] = currentYear;
 
-          statistic = await Statistic.findByIdAndUpdate(statistic._id, {"statistics.chartFour" : dataChartFour});
+          statistic = await Statistic.findByIdAndUpdate(statistic._id, {
+            "statistics.chartFour": dataChartFour,
+          });
         }
       }
     });
-
   } catch (error) {
     loggerError.error(error);
-    NodeMailer.send(error, 'Une erreur est survenue dans la fonction statisticChartFour!');
   }
 };
