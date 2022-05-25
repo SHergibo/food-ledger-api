c019ccd08e17808512efdf7a6236ab80adac90c3
SHergibo <sachau888@hotmail.com>
2022-05-25 14:34:38 +0200

    feat: Created sendGrid-mail.helper.js.

---
+++ api/helpers/sendGrid-mail.helper.js
@@ -0,0 +1,27 @@
+const {
+    SendGridAPIKey,
+    SendGridFrom,
+    SendGridListTemplateId,
+    SendGridBaseTemplateId,
+  } = require("../../config/environment.config"),
+  sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(SendGridAPIKey);
+
+exports.sendGridEmail = async ({ to, templateType, dynamic_template_data }) => {
+  try {
+    const msg = {
+      to,
+      from: SendGridFrom,
+      templateId:
+        templateType === "list"
+          ? SendGridListTemplateId
+          : SendGridBaseTemplateId,
+      dynamic_template_data,
+    };
+    await sgMail.send(msg);
+    return;
+  } catch (error) {
+    return error;
+  }
+};
