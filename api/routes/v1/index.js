d0e15a2ee893b86eeed18f854969bbc61f110b59
SHergibo <sachau888@hotmail.com>
2022-06-03 14:59:03 +0200

    feat: Added /request-reset-password route in index in /routes/v1 foler.

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -1,30 +1,32 @@
-const router = require('express').Router(),
-      UserRoutes = require('./user.route'),
-      OptionRoutes = require('./option.route'),
-      HouseholdRoutes = require('./household.route'),
-      NotificationRoutes = require('./notification.route'),
-      RequestRoutes = require('./request.route'),
-      ProductRoutes = require('./product.route'),
-      HistoricRoutes = require('./historic.route'),
-      BrandRoutes = require('./brand.route'),
-      ProductLogRoutes = require('./product-log.route'),
-      ShoppingListRoutes = require('./shopping-list.route'),
-      StatisticRoutes = require('./statistic.route'),
-      AuthRoutes = require('./auth.route');
-      // ScriptRoutes = require('./script.route');
+const router = require("express").Router(),
+  UserRoutes = require("./user.route"),
+  OptionRoutes = require("./option.route"),
+  HouseholdRoutes = require("./household.route"),
+  NotificationRoutes = require("./notification.route"),
+  RequestRoutes = require("./request.route"),
+  ProductRoutes = require("./product.route"),
+  HistoricRoutes = require("./historic.route"),
+  BrandRoutes = require("./brand.route"),
+  ProductLogRoutes = require("./product-log.route"),
+  ShoppingListRoutes = require("./shopping-list.route"),
+  StatisticRoutes = require("./statistic.route"),
+  AuthRoutes = require("./auth.route"),
+  EmailResetPasswordAuthToken = require("./emailResetPasswordAuthToken.route");
+// ScriptRoutes = require('./script.route');
 
-router.use('/auth', AuthRoutes);
-router.use('/users', UserRoutes);
-router.use('/options', OptionRoutes);
-router.use('/households', HouseholdRoutes);
-router.use('/notifications', NotificationRoutes);
-router.use('/requests', RequestRoutes);
-router.use('/products', ProductRoutes);
-router.use('/historics', HistoricRoutes);
-router.use('/brands', BrandRoutes);
-router.use('/product-logs', ProductLogRoutes);
-router.use('/shopping-lists', ShoppingListRoutes);
-router.use('/statistics', StatisticRoutes);
+router.use("/auth", AuthRoutes);
+router.use("/users", UserRoutes);
+router.use("/options", OptionRoutes);
+router.use("/households", HouseholdRoutes);
+router.use("/notifications", NotificationRoutes);
+router.use("/requests", RequestRoutes);
+router.use("/products", ProductRoutes);
+router.use("/historics", HistoricRoutes);
+router.use("/brands", BrandRoutes);
+router.use("/product-logs", ProductLogRoutes);
+router.use("/shopping-lists", ShoppingListRoutes);
+router.use("/statistics", StatisticRoutes);
+router.use("/request-reset-password", EmailResetPasswordAuthToken);
 // router.use('/scripts', ScriptRoutes);
 
 module.exports = router;
